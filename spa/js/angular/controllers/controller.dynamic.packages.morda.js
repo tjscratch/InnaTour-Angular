cf7c6d935a1dec5619edb499eced807f23ad1468
Greg Karelin <grundiss@gmail.com>
2014-04-04 19:31:59 +0400

    dynamic packages controllers to separate files

--- spa/js/angular/controllers/controller.dynamic.packages.morda.js
+++ spa/js/angular/controllers/controller.dynamic.packages.morda.js
@@ -1,8 +1,6 @@
 innaAppControllers.
     controller('DynamicPackageMordaCtrl', ['$scope', 'DynamicPackagesDataProvider',
         function ($scope, DynamicPackagesDataProvider) {
-            $scope.$on('inna.DynamicPackages.Search', function(event, data){
-                console.log(data);
-            });
+            
         }
     ]);
