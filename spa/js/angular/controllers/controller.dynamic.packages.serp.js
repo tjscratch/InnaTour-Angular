f75a9411f5277ebd46a0146ad4aed4747379356c
Greg Karelin <grundiss@gmail.com>
2014-04-09 12:27:35 +0400

    Dynamic serp stub

--- spa/js/angular/controllers/controller.dynamic.packages.serp.js
+++ spa/js/angular/controllers/controller.dynamic.packages.serp.js
@@ -5,18 +5,20 @@ innaAppControllers.
             /*EventListener*/
             DynamicFormSubmitListener.listen();
 
-            $scope.combinations = {};
+            /*Properties*/
+            $scope.combinations = [];
             $scope.hotels = {};
             $scope.tickets = {};
 
             $scope.showLanding = true;
 
+            /*Data fetching*/
             (function loadData(params){
                 params.StartVoyageDate = dateHelper.ddmmyyyy2yyyymmdd(params.StartVoyageDate);
                 params.EndVoyageDate = dateHelper.ddmmyyyy2yyyymmdd(params.EndVoyageDate);
 
                 console.time('loading_packages');
-                console.log('loading data by params', params);
+                console.log('loading data by params', angular.toParam(params));
 
                 DynamicPackagesDataProvider.search(params, function(data){
                     console.timeEnd('loading_packages');
@@ -26,13 +28,28 @@ innaAppControllers.
 
                         $scope.combinations = data.Combinations;
                         $scope.hotels = data.Hotels;
-                        $scope.tickets = data.Tickets;$scope.showLanding = false;
+                        $scope.tickets = data.Tickets;
+
+                        $scope.showLanding = false;
+
+                        $scope.currentCombination = $scope.combinations[0];
 
                         console.timeEnd('rendering');
                     });
                 });
-
-
             })(angular.copy($routeParams));
+
+            /*Methods*/
+            $scope.getTicketByCombination = function(combination) {
+                console.log('all tickets = ', $scope.tickets);
+                console.log('combination = ', combination);
+                return 'ticket';
+            }
+
+            $scope.getHotelByCombination = function(combination) {
+                console.log('all hotels = ', $scope.hotels);
+                console.log('combination = ', combination);
+                return 'hotel';
+            }
         }
     ]);
