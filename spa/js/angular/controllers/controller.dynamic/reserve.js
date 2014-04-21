853841c4eabbcbdd8faadc6b1c172359a1cc14e2
Greg Karelin <grundiss@gmail.com>
2014-04-21 17:44:02 +0400

    Dynamic: Reservation (stub)

--- spa/js/angular/controllers/controller.dynamic/reserve.js
+++ spa/js/angular/controllers/controller.dynamic/reserve.js
@@ -1,11 +1,11 @@
 angular.module('innaApp.controllers')
     .controller('DynamicReserveTicketsCtrl', [
-        '$scope', '$controller',
-        function($scope, $controller){
-            $controller('AviaReserveTicketsCtrl', {$scope: $scope});
+        '$scope', '$controller', '$routeParams',
+        function($scope, $controller, $routeParams){
+            $controller('ReserveTicketsCtrl', { $scope: $scope });
 
             $scope.objectToReserveTemplate = '/spa/templates/pages/dynamic/inc/reserve.html';
 
-            console.log('hi from DynamicReserveTicketsCtrl');
+            console.log('hi from DynamicReserveTicketsCtrl', $routeParams);
         }
     ]);
