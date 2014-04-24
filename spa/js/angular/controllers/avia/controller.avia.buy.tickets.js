44906e7717e71d89cf4bb72af4deb1fa272c0af5
Sveshnikov Alexander <devnull@localhost>
2014-04-24 20:16:48 +0400

    IN-764 Валидация парамтеров и определения типа банковской карты

--- spa/js/angular/controllers/avia/controller.avia.buy.tickets.js
+++ spa/js/angular/controllers/avia/controller.avia.buy.tickets.js
@@ -20,19 +20,165 @@ innaAppControllers.
             $scope.searchId = $scope.criteria.QueryId;
             $scope.reservationModel = null;
             $scope.payModel = {
-                num: {
-                    num1: '1345',
-                    num2: '2322',
-                    num3: '3456',
-                    num4: '4876'
-                    
-                },
+                num1: '1345',
+                num2: '2322',
+                num3: '3456',
+                num4: '4876',
                 cvc2: '123',
                 cardHolder: 'Ivan Ivanov',
                 cardMonth: '02',
                 cardYear: '15',
-                agree: true
+                agree: false
             };
+
+            function initValidateModel() {
+                $scope.isValid = {
+                    num1: true,
+                    num2: true,
+                    num3: true,
+                    num4: true,
+                    cvc2: true,
+                    cardHolder: true,
+                    cardMonth: true,
+                    cardYear: true,
+                    agree: true
+                };
+
+                function validateNum() {
+                    function setNums(isValid) {
+                        $scope.isValid.num1 = isValid;
+                        $scope.isValid.num2 = isValid;
+                        $scope.isValid.num3 = isValid;
+                        $scope.isValid.num4 = isValid;
+                    }
+                    var cardNum = $scope.payModel.num1 + $scope.payModel.num2 + $scope.payModel.num3 + $scope.payModel.num4;
+                    if (cardNum.length == 16) {
+                        setNums(true);
+                        return true;
+                    }
+                    setNums(false);
+                    return false;
+                }
+
+                $scope.validate = {
+                    num1: function () {
+                        return validateNum();
+                    },
+                    num2: function () {
+                        return validateNum();
+                    },
+                    num3: function () {
+                        return validateNum();
+                    },
+                    num4: function () {
+                        return validateNum();
+                    },
+                    cvc2: function validateCvv() {
+                        if ($scope.payModel.cvc2.length == 3) {
+                            $scope.isValid.cvc2 = true;
+                            return true;
+                        }
+                        $scope.isValid.cvc2 = false;
+                        return false;
+                    },
+                    cardHolder: function validateCardholder() {
+                        if ($scope.payModel.cardHolder.length > 0) {
+                            $scope.isValid.cardHolder = true;
+                            return true;
+                        }
+                        $scope.isValid.cardHolder = false;
+                        return false;
+                    },
+                    cardMonth: function validateCardMonth() {
+                        if ($scope.payModel.cardMonth.length > 0) {
+                            var iMonth = parseInt($scope.payModel.cardMonth);
+                            if (iMonth >= 1 && iMonth <= 12) {
+                                $scope.isValid.cardMonth = true;
+                                return true;
+                            }
+                        }
+                        $scope.isValid.cardMonth = false;
+                        return false;
+                    },
+                    cardYear: function validateCardYear() {
+                        if ($scope.payModel.cardYear.length > 0) {
+                            var iYear = parseInt($scope.payModel.cardYear);
+                            if (iYear >= 14) {
+                                $scope.isValid.cardYear = true;
+                                return true;
+                            }
+                        }
+                        $scope.isValid.cardYear = false;
+                        return false;
+                    },
+                    agree: function () {
+                        $scope.isValid.agree = $scope.payModel.agree;
+                        return $scope.isValid.agree;
+                    }
+                };
+            }
+            initValidateModel();
+
+            $scope.validateError = function () {
+                this.field = '';
+                this.isValid = false;
+            }
+
+            function showPopupErr(id) {
+                var $to = $('#' + id);
+                if ($to.attr('tt') != 'true') {
+                    $to.attr('tt', 'true');
+                    $to.tooltipX({ autoShow: false, autoHide: false, position: { my: 'center top+22', at: 'center bottom' } });
+                }
+                $to.tooltipX("open");
+            }
+
+            function closeErrPopups() {
+                _.each(_.keys($scope.validate), function (key) {
+                    var $to = $('#' + key);
+                    if ($to.attr('tt') == 'true') {
+                        console.log('closeErrPopups, id: ' + key);
+                        $to.tooltipX("close");
+                    }
+                });
+            }
+
+            $scope.$watch('payModel', function () {
+                closeErrPopups();
+                validateKeys();
+            }, true);
+
+            function validateKeys() {
+                _.each(_.keys($scope.validate), function (key) {
+                    var fn = $scope.validate[key];
+                    if (fn != null)
+                        fn();
+                });
+            }
+
+            function validate() {
+                validateKeys();
+
+                var isValid = _.all(_.keys($scope.isValid), function (key) {
+                    return $scope.isValid[key] == true;
+                });
+                return isValid;
+            }
+
+            function validateAndShowPopup() {
+                validate();
+
+                var keys = _.keys($scope.isValid);
+                for (var i = 0; i < keys.length; i++) {
+                    var key = keys[i];
+                    if ($scope.isValid[key] == false) {
+                        showPopupErr(key);
+                        return false;
+                    }
+                }
+
+                return true;
+            }
 
             $scope.sexType = aviaHelper.sexType;
 
@@ -138,8 +284,11 @@ innaAppControllers.
             $scope.processToBuy = function ($event) {
                 eventsHelper.preventBubbling($event);
 
-                if ($scope.payModel.agree && isAllDataLoaded()) {
-                    var cardNum = $scope.payModel.num.num1 + $scope.payModel.num.num2 + $scope.payModel.num.num3 + $scope.payModel.num.num4;
+                if (validateAndShowPopup()) {
+
+                    return;
+
+                    var cardNum = $scope.payModel.num1 + $scope.payModel.num2 + $scope.payModel.num3 + $scope.payModel.num4;
 
                     var apiPayModel = {
                         OrderNum: $scope.criteria.OrderNum,
