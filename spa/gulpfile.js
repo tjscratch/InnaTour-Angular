25da82c87b77fac9e2f805cf923117b59388b9df
Tagir Yusupov <tagir.yusupov@gmail.com>
2014-05-07 12:35:10 +0400

    fix gulp

--- spa/gulpfile.js
+++ spa/gulpfile.js
@@ -10,41 +10,37 @@ var gulp = require('gulp'),
 
 gulp.task('styles', function () {
     gulp.src(['styl/common.styl'])
+        .pipe(stylus({
+            use: ['nib'],
+            import: ['nib']
+        }))
         .pipe(concat('tickets.min.css'))
-        .pipe(stylus({
-            use: ['nib'],
-            import: ['nib']
-        }))
         .pipe(minifyCSS(opts))
-        .pipe(gulp.dest('css'))
-        // .pipe(livereload());
+        .pipe(gulp.dest('css'));
     gulp.src(['styl/ie.styl'])
+        .pipe(stylus({
+            use: ['nib'],
+            import: ['nib']
+        }))
         .pipe(concat('ie.min.css'))
-        .pipe(stylus({
-            use: ['nib'],
-            import: ['nib']
-        }))
         .pipe(minifyCSS(opts))
-        .pipe(gulp.dest('css'))
-        // .pipe(livereload());
+        .pipe(gulp.dest('css'));
     gulp.src(['styl/ticket.styl'])
-        .pipe(concat('ticket.min.css'))
         .pipe(stylus({
             use: ['nib'],
             import: ['nib']
         }))
+        .pipe(concat('ticket.min.css'))
         .pipe(minifyCSS(opts))
-        .pipe(gulp.dest('css'))
-        // .pipe(livereload());
+        .pipe(gulp.dest('css'));
     gulp.src(['css/main/*.less', 'css/pages/*.less'])
         .pipe(concat('main.css'))
         .pipe(less())
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function () {
     var server = livereload();
-    // gulp.watch('js/*.js', ['scripts']);
     gulp.watch('styl/*.styl', ['styles']);
     gulp.watch('*.php', function(evt) {
         server.changed(evt.path);
