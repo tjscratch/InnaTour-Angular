5fcec3327c7928f4e3f9ac29cbe564af7998827e
'Valentine  Titov' randomaile <'Valentine  Titov' randomaile@gmail.com>
2014-05-14 09:09:27 +0400

    changes

---
+++ spa/js/recomend-bundle.js
@@ -0,0 +1,28 @@
+$(function () {
+    var isCollapse = false;
+    var recommendBundle = $('.recommend-bundle');
+    var scrollTop
+
+    if (recommendBundle.length) {
+        isCollapse
+
+        scrollTop = $(document.body).scrollTop();
+
+        $(window).on('scroll', function (evt) {
+
+
+            if (scrollTop > 20) {
+
+            }
+        });
+
+    }
+
+    function collapse () {
+
+    }
+
+    function expand () {
+
+    }
+});
