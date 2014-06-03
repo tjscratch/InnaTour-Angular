2aa0f731aa04a7a2012aac0d7b35364f6aa82f55
Greg Karelin <grundiss@gmail.com>
2014-06-03 19:18:19 +0400

    IN-1274

--- spa/js/angular/controllers/controller.dynamic/serp.js
+++ spa/js/angular/controllers/controller.dynamic/serp.js
@@ -225,6 +225,7 @@ innaAppControllers
                             hotel.detailed = resp;
 
                             serpScope.$broadcast(Events.DYNAMIC_SERP_HOTEL_DETAILS_LOADED);
+                            serpScope.$digest();
                         }
                     );
                 }
