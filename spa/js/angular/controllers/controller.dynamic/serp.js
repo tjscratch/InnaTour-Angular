173344533d71edbc5b9db4c2741e63b30a73772f
Dmitry Kuznetsov <batmandarkside@gmail.com>
2014-06-03 18:22:07 +0400

    убрал console.log

--- spa/js/angular/controllers/controller.dynamic/serp.js
+++ spa/js/angular/controllers/controller.dynamic/serp.js
@@ -59,7 +59,7 @@ innaAppControllers
                 if (!method || !param) return;
 
                 ServiceDynamicPackagesDataProvider[method](param, searchParams, function (data) {
-                    console.log(data, 'data');
+                    //console.log(data, 'data');
                     $scope.$apply(function ($scope) {
                         apply($scope, data);
                         deferred.resolve();
