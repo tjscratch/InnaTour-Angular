81d61d3a8134ff00ddd03e69475255c46e3d0140
Dmitry Kuznetsov <batmandarkside@gmail.com>
2014-06-03 18:19:27 +0400

    вывел адрес в карточке отеля

--- spa/js/angular/controllers/controller.dynamic/serp.js
+++ spa/js/angular/controllers/controller.dynamic/serp.js
@@ -49,6 +49,7 @@ innaAppControllers
                 if (!method || !param) return;
 
                 ServiceDynamicPackagesDataProvider[method](param, searchParams, function (data) {
+                    console.log(data, 'data');
                     $scope.$apply(function ($scope) {
                         apply($scope, data);
                         deferred.resolve();
