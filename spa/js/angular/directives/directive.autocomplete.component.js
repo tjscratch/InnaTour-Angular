b883145438921485fdc91df8ea109c56c32232d5
Dmitry Kuznetsov <batmandarkside@gmail.com>
2014-06-16 10:47:49 +0400

    test

--- spa/js/angular/directives/directive.autocomplete.component.js
+++ spa/js/angular/directives/directive.autocomplete.component.js
@@ -1,133 +1,140 @@
-﻿﻿innaAppDirectives.directive('dropdownInput', ['$templateCache', 'eventsHelper', function ($templateCache, eventsHelper) {
-    return {
-        replace: true,
-        template: $templateCache.get('components/dropdown_input.html'),
-        scope: {
-            provideSuggestCallback: '=', //callback for ngChange
-            suggest: '=', //list of suggested objects
-            result: '=',
-            setResultCallback: '&',
-            setResultItem: '=',
-            theme: '@',
-            askForData: '=',
-            placeholder: '@'
-        },
-        controller: ['$scope', function($scope){
-            /*Properties*/
-            $scope.fulfilled = false;
-
-            $scope.getPlaceholder = function () {
-                if ($scope.placeholder == null || $scope.placeholder.length == 0)
-                    return 'Откуда';
-                else
-                    return $scope.placeholder;
-            }
-
-            $scope.doResultCallback = function (item, city) {
-                if ($scope.setResultCallback) {
-                    //var dataItem = { 'item': option, 'city': option };
-                    $scope.setResultCallback({ 'item': item, 'city': city });
-                }
-            }
-
-            //эта хуйня нужна чтобы можно было присвоить и id и name сразу, без доп запросов
-            var unwatch = $scope.$watch('setResultItem', function (item) {
-                if (item != null) {
-                    init(item);
-                    //unwatch();
-                }
-            }, true);
-
-            function init(item) {
-                if ($scope.input) {
-                    $scope.input.val(item.Name);
-                }
-                $scope.result = item.Id;
-            }
-
-            /*Events*/
-            $scope.setCurrent = function ($event, option, airport) {
-                //запрещаем баблинг
-                $event && eventsHelper.preventBubbling($event);
-
-                if (option != null) {
-                    if (airport != null) {
-                        $scope.input.val(airport.Name);
-                        $scope.result = airport.Id;
-                        $scope.doResultCallback(airport, option);
-                    }
-                    else {
-                        $scope.input.val(option.Name);
-                        $scope.result = option.Id;
-                        $scope.doResultCallback(option, null);
-                    }
-                }
-                $scope.fulfilled = true;
-            }
-            $scope.unfulfill = function(){
+﻿innaAppDirectives.directive('dropdownInput', [
+    '$templateCache',
+    'eventsHelper',
+    function ($templateCache, eventsHelper) {
+        return {
+            replace: true,
+            template: $templateCache.get('components/dropdown_input.html'),
+            scope: {
+                provideSuggestCallback: '=', //callback for ngChange
+                suggest: '=', //list of suggested objects
+                result: '=',
+                setResultCallback: '&',
+                setResultItem: '=',
+                theme: '@',
+                askForData: '=',
+                placeholder: '@'
+            },
+            controller: ['$scope', function ($scope) {
+                /*Properties*/
                 $scope.fulfilled = false;
-                $scope.result = null;
-            }
 
-            /*Watchers*/
-            $scope.$watch('result', function (newValue, oldValue) {
-                //console.log('$scope.$watch(result: %s', newValue);
-                if(newValue instanceof Error) {
-                    $scope.result = oldValue;
+                $scope.getPlaceholder = function () {
+                    if ($scope.placeholder == null || $scope.placeholder.length == 0)
+                        return 'Откуда';
+                    else
+                        return $scope.placeholder;
+                }
 
-                    $scope.input.tooltip({
-                        position: {
-                            my: 'center top+22',
-                            at: 'center bottom'
-                        },
-                        items: "[data-title]",
-                        content: function () {
-                            return $scope.input.data("title");
-                        }
-                    }).tooltip('open');
-                } else if(!$scope.input.val()) {
-                    if(newValue != null && newValue != 'null' && $scope.askForData) {
-                        $scope.askForData(newValue, function (data) {
-                            $scope.setCurrent(null, data);
-                        });
+                $scope.doResultCallback = function (item, city) {
+                    if ($scope.setResultCallback) {
+                        //var dataItem = { 'item': option, 'city': option };
+                        $scope.setResultCallback({ 'item': item, 'city': city });
                     }
                 }
-            });
-        }],
-        link: function(scope, elem, attrs){
-            scope.input = $('input[type="text"]', elem);
 
-            /*Events*/
-            scope.input.keyup(function(event){
-                var value = scope.input.val();
-                var preparedText = value.split(', ')[0].trim();
+                //эта хуйня нужна чтобы можно было присвоить и id и name сразу, без доп запросов
+                var unwatch = $scope.$watch('setResultItem', function (item) {
+                    if (item != null) {
+                        init(item);
+                        //unwatch();
+                    }
+                }, true);
 
-                if(preparedText.length) {
-                    scope.provideSuggestCallback(preparedText, value);
+                function init(item) {
+                    if ($scope.input) {
+                        $scope.input.val(item.Name);
+                    }
+                    $scope.result = item.Id;
                 }
-            });
 
-            scope.input.focus(function () {
-                scope.$apply(function($scope){
+                /*Events*/
+                $scope.setCurrent = function ($event, option, airport) {
+                    //запрещаем баблинг
+                    $event && eventsHelper.preventBubbling($event);
+
+                    if (option != null) {
+                        if (airport != null) {
+                            $scope.input.val(airport.Name);
+                            $scope.result = airport.Id;
+                            $scope.doResultCallback(airport, option);
+                        }
+                        else {
+                            $scope.input.val(option.Name);
+                            $scope.result = option.Id;
+                            $scope.doResultCallback(option, null);
+                        }
+                    }
+                    $scope.fulfilled = true;
+                }
+
+                $scope.unfulfill = function () {
+                    console.log('keypress');
                     $scope.fulfilled = false;
+                    $scope.result = null;
+                }
+
+                /*Watchers*/
+                $scope.$watch('result', function (newValue, oldValue) {
+                    console.log('$scope.$watch(result: %s', newValue);
+                    if (newValue instanceof Error) {
+                        $scope.result = oldValue;
+
+                        $scope.input.tooltip({
+                            position: {
+                                my: 'center top+22',
+                                at: 'center bottom'
+                            },
+                            items: "[data-title]",
+                            content: function () {
+                                return $scope.input.data("title");
+                            }
+                        }).tooltip('open');
+                    } else if (!$scope.input.val()) {
+                        if (newValue != null && newValue != 'null' && $scope.askForData) {
+                            $scope.askForData(newValue, function (data) {
+                                $scope.setCurrent(null, data);
+                            });
+                        }
+                    }
+                });
+            }],
+
+            link: function (scope, elem, attrs) {
+                scope.input = $('input[type="text"]', elem);
+
+                /*Events*/
+                scope.input.keyup(function (event) {
+                    var value = scope.input.val();
+                    var preparedText = value.split(', ')[0].trim();
+
+                    if (preparedText.length) {
+                        scope.provideSuggestCallback(preparedText, value);
+                    }
                 });
 
-                try{
-                    scope.input.tooltip('destroy');
-                } catch(e) {}
-            });
-
-            $(document).click(function (event) {
-                var isInsideComponent = !!$(event.target).closest(elem).length;
-
-                if (!isInsideComponent) {
+                scope.input.focus(function () {
                     scope.$apply(function ($scope) {
-                        scope.fulfilled = true;
-                        //select all
-                        $(event.target).select();
+                        $scope.fulfilled = false;
                     });
-                }
-            });
+
+                    try {
+                        scope.input.tooltip('destroy');
+                    } catch (e) {
+                    }
+                });
+
+                $(document).click(function (event) {
+                    var isInsideComponent = !!$(event.target).closest(elem).length;
+
+                    if (!isInsideComponent) {
+                        scope.$apply(function ($scope) {
+                            scope.fulfilled = true;
+                            //select all
+                            $(event.target).select();
+                        });
+                    }
+                });
+            }
         }
-    }
-}]);
+    }]);
