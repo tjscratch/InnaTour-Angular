64239c471dd77a33bd16fcc124ea9ec63108a048
Dmitry Kuznetsov <batmandarkside@gmail.com>
2014-07-02 15:54:22 +0400

    сделал нескольно новых partials

---
+++ spa/js/angular/components/dynamic-block/avia-hotel.js
@@ -0,0 +1,23 @@
+innaAppConponents.
+    factory('dynamicBlock', [
+        'innaApp.API.events',
+        '$templateCache',
+        function (Events, $templateCache) {
+
+            var dynamicBlock = Ractive.extend({
+                debug: true,
+                template: $templateCache.get('components/dynamic-block/templ/avia-hotel.html'),
+                partials: {
+                    ticket2ways: $templateCache.get('components/ticket/templ/ticket2ways.html'),
+                    partialInfoHotel: $templateCache.get('components/hotel/templ/partial-info.html')
+                },
+                init: function () {
+                    console.log('avia-hotel.html');
+                    console.log(this.get('Hotel'));
+                }
+            });
+
+
+            return dynamicBlock;
+        }
+    ])
