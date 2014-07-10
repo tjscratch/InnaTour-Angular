52c0f34961bf25850716716d2b30fa9fc179c6ed
Greg Karelin <grundiss@gmail.com>
2014-07-10 13:57:32 +0400

    Grid sys

--- spa/js/angular/controllers/controller.offers.list.js
+++ spa/js/angular/controllers/controller.offers.list.js
@@ -51,11 +51,11 @@ innaAppControllers.
             }
 
             $scope.hasImgFront = function(item){
-                return !$scope.isNullOrEmpty(item.BackTitleRow1) ||
-                !$scope.isNullOrEmpty(item.BackTitleRow2) ||
-                !$scope.isNullOrEmpty(item.BackSubTitleRow1) ||
-                !$scope.isNullOrEmpty(item.BackSubTitleRow2) ||
-                !$scope.isNullOrEmpty(item.BackPrice)
+                return !$scope.isNullOrEmpty(item.FrontTitleRow1) ||
+                !$scope.isNullOrEmpty(item.FrontTitleRow2) ||
+                !$scope.isNullOrEmpty(item.FrontSubTitleRow1) ||
+                !$scope.isNullOrEmpty(item.FrontSubTitleRow2) ||
+                !$scope.isNullOrEmpty(item.FrontPrice)
             }
 
 
