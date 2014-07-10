bf0ad7273d40ae96eb6789107b7b19292a18e00e
Greg Karelin <grundiss@gmail.com>
2014-07-10 19:37:58 +0400

    HelpPage::toggler

--- spa/js/angular/pages/page-help/page-help.ctrl.js
+++ spa/js/angular/pages/page-help/page-help.ctrl.js
@@ -4,15 +4,43 @@ angular.module('innaApp.controllers')
         '$templateCache',
         'HelpDataService',
         function($scope, $templateCache, HelpDataService){
+
+            var toggler = Ractive.extend({
+                debug: true,
+                template: $templateCache.get('pages/page-help/templ/page-help-toggle.html'),
+                data : {
+                    visible : false
+                },
+                init: function(){
+
+                    this.on({
+                        toggleVisible : this.toggleVisible
+                    })
+                },
+                toggleVisible : function(){
+                    this.toggle('visible')
+                }
+            });
+
             new (Ractive.extend({
                 debug: true,
                 el: document.querySelector('.page-help'),
                 template: $templateCache.get('pages/page-help/templ/page-help.html'),
+                data: {
+                    topics: null
+                },
+                components : {
+                    Toggle : toggler
+                },
                 init: function(){
                     var self = this;
 
                     HelpDataService.fetchAll(function(data){
                         console.log(data);
+
+                        self.set({
+                            topics: data
+                        })
                     });
                 }
             }));
