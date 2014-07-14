f3d666aa1c51b8bac91dc7e7d93147d8bd402680
Dmitry Kuznetsov <batmandarkside@gmail.com>
2014-07-14 16:17:18 +0400

    непного расширил ajax.helper

--- spa/js/angular/ang.helpers/ajax.helper.js
+++ spa/js/angular/ang.helpers/ajax.helper.js
@@ -1,122 +1,5 @@
 angular.module('innaApp.services')
     .factory('AjaxHelper', [
-        function(){
-            var ajax = {};
-            var cache = {};
-
-            function doAjax(options) {
-                return $.ajax(options);
-            }
-
-            function buildOptions(url, data, method, useCache) {
-                var o = {
-                    url: url,
-                    type: method,
-                    dataType: 'json',
-                    traditional: !hasObjects(data),
-                    data: data,
-                    //xhrFields: { withCredentials: true },
-                    //crossDomain: true,
-                    //async: typeof async !== 'undefined' ? async : true,
-                    eol: null
-                };
-
-                if (typeof useCache !== 'undefined'){
-                    o.cache = useCache;
-                }
-
-                //if (async == false) {
-                //    //при синхронных вызовах последний FF ругается и блочит запрос, нужно удалить withCredentials
-                //    delete o.xhrFields;
-                //}
-                return o;
-            }
-
-            function hasObjects(data) {
-                for(var key in data) if(data.hasOwnProperty(key)){
-                    if(_.isObject(data[key])) return true;
-                }
-
-                return false;
-            }
-
-            ajax.cancelRequest = function (url) {
-                if (cache[url]) {
-                    cache[url].abort();
-                }
-            };
-
-            ajax.getCancelable = function (url, data, success, error) {
-                var request = doAjax(buildOptions(url, data, 'GET', false));
-
-                request.done(success || angular.noop).fail(error || angular.noop).always(function () {
-                    delete cache[url];
-                });
-
-                cache[url] = request;
-
-                return request;
-            };
-
-            ajax.getNoCache = function (url, data, success, error) {
-                var request = doAjax(buildOptions(url, data, 'GET', false));
-
-                request.done(success || angular.noop).fail(error || angular.noop);
-
-                return request;
-            };
-
-            ajax.get = function (url, data, success, error, useCache) {
-                var request = doAjax(buildOptions(url, data, 'GET', useCache));
-
-                request.done(success || angular.noop).fail(error || angular.noop);
-
-                return request;
-            };
-
-            ajax.post = function (url, data, success, error, useCache) {
-                var request = doAjax(buildOptions(url, data, 'POST', useCache));
-
-                request.done(success || angular.noop).fail(error || angular.noop);
-
-                return request;
-            };
-
-            ajax.getDebounced = function (url, data, success, error, useCache) {
-                if(cache[url]) {
-                    cache[url].abort();
-                }
-
-                var req = ajax.get(url, data, success, error, useCache).always(function () {
-                    delete cache[url];
-                });
-
-                cache[url] = req;
-
-                return req;
-            };
-
-            ajax.postDebaunced = function (url, data, success, error, useCache) {
-                if(cache[url]) {
-                    cache[url].abort();
-                }
-
-                var req = ajax.post(url, data, success, error, useCache).always(function () {
-                    delete cache[url];
-                });
-
-                cache[url] = req;
-
-                return req;
-            };
-
-            return ajax;
-        }
-    ]);
-
-/*
-angular.module('innaApp.services')
-    .factory('AjaxHelper', [
         '$templateCache',
         'ErrorManager',
         function ($templateCache, ErrorManager) {
@@ -142,7 +25,7 @@ angular.module('innaApp.services')
 
                 var async = $.ajax({
                     url: params.url,
-                    type: params.method,
+                    type: params.type,
                     dataType: 'json',
                     cache : params.cache || false,
                     traditional: !hasObjects(params.data),
@@ -293,5 +176,121 @@ angular.module('innaApp.services')
             return ajax;
         }
     ]);
-*/
 
+
+
+/*angular.module('innaApp.services')
+ .factory('AjaxHelper', [
+ function(){
+ var ajax = {};
+ var cache = {};
+
+ function doAjax(options) {
+ return $.ajax(options);
+ }
+
+ function buildOptions(url, data, method, useCache) {
+ var o = {
+ url: url,
+ type: method,
+ dataType: 'json',
+ traditional: !hasObjects(data),
+ data: data,
+ //xhrFields: { withCredentials: true },
+ //crossDomain: true,
+ //async: typeof async !== 'undefined' ? async : true,
+ eol: null
+ };
+
+ if (typeof useCache !== 'undefined'){
+ o.cache = useCache;
+ }
+
+ //if (async == false) {
+ //    //при синхронных вызовах последний FF ругается и блочит запрос, нужно удалить withCredentials
+ //    delete o.xhrFields;
+ //}
+ return o;
+ }
+
+ function hasObjects(data) {
+ for(var key in data) if(data.hasOwnProperty(key)){
+ if(_.isObject(data[key])) return true;
+ }
+
+ return false;
+ }
+
+ ajax.cancelRequest = function (url) {
+ if (cache[url]) {
+ cache[url].abort();
+ }
+ };
+
+ ajax.getCancelable = function (url, data, success, error) {
+ var request = doAjax(buildOptions(url, data, 'GET', false));
+
+ request.done(success || angular.noop).fail(error || angular.noop).always(function () {
+ delete cache[url];
+ });
+
+ cache[url] = request;
+
+ return request;
+ };
+
+ ajax.getNoCache = function (url, data, success, error) {
+ var request = doAjax(buildOptions(url, data, 'GET', false));
+
+ request.done(success || angular.noop).fail(error || angular.noop);
+
+ return request;
+ };
+
+ ajax.get = function (url, data, success, error, useCache) {
+ var request = doAjax(buildOptions(url, data, 'GET', useCache));
+
+ request.done(success || angular.noop).fail(error || angular.noop);
+
+ return request;
+ };
+
+ ajax.post = function (url, data, success, error, useCache) {
+ var request = doAjax(buildOptions(url, data, 'POST', useCache));
+
+ request.done(success || angular.noop).fail(error || angular.noop);
+
+ return request;
+ };
+
+ ajax.getDebounced = function (url, data, success, error, useCache) {
+ if(cache[url]) {
+ cache[url].abort();
+ }
+
+ var req = ajax.get(url, data, success, error, useCache).always(function () {
+ delete cache[url];
+ });
+
+ cache[url] = req;
+
+ return req;
+ };
+
+ ajax.postDebaunced = function (url, data, success, error, useCache) {
+ if(cache[url]) {
+ cache[url].abort();
+ }
+
+ var req = ajax.post(url, data, success, error, useCache).always(function () {
+ delete cache[url];
+ });
+
+ cache[url] = req;
+
+ return req;
+ };
+
+ return ajax;
+ }
+ ]);*/
