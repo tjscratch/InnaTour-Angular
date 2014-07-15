e99f7922337d1867bf7826c26562729fc38df501
Greg Karelin <grundiss@gmail.com>
2014-07-15 12:32:03 +0400

    Done

--- node_tasks/replace.js
+++ node_tasks/replace.js
@@ -13,9 +13,8 @@ var staticHost = (_ENV_ === 'production') ? conf.hosts.static.prod : conf.hosts.
 
 var __PROTOCOL__ = (_ENV_ === 'production') ? conf.protocol.https : conf.protocol.http;
 
-// Копируем в папку publish
-gulp.task('replace-index', function () {
-    var confReplace = {
+function getConfReplace(){
+    return {
         'app-main-js': '/'+ conf.version +'/js/app-main.js',
         'app-less': '/'+ conf.version +'/css/main.less.css',
         'app-stylus': '/'+ conf.version +'/css/common.min.css',
@@ -25,32 +24,29 @@ gulp.task('replace-index', function () {
         'front-host': 'app_main.frontHost = \'' + apiFrontHost + '\';',
         'static-host': 'app_main.staticHost = \'' + staticHost + '\';',
         'tripadvisor': 'app_main.tripadvisor = \'' + __PROTOCOL__ + conf.tripadvisor + '\';'
-    };
+    }
+}
 
+// Копируем в папку publish
+gulp.task('replace-index', function () {
     return gulp.src('./index.html')
-        .pipe(htmlreplace(confReplace))
+        .pipe(htmlreplace(getConfReplace()))
         .pipe(gulp.dest(conf.publish));
+});
+
+gulp.task('replace-browser', function () {
+    return gulp.src('./spa/browser.html')
+        .pipe(htmlreplace(getConfReplace()))
+        .pipe(gulp.dest(conf.publish + '/spa'));
 });
 
 
 // Копируем в папку publish/tours
 gulp.task('release-tours', function () {
-
-    var confReplace = {
-        'app-main-js': '/'+ conf.version +'/js/app-main.js',
-        'app-less': '/'+ conf.version +'/css/main.less.css',
-        'app-stylus': '/'+ conf.version +'/css/common.min.css',
-
-        'app-host': 'app_main.host = \'' + apiHost + '\';',
-        'b2b-host': 'app_main.b2bHost = \'' + b2bHost + '\';',
-        'front-host': 'app_main.frontHost = \'' + apiFrontHost + '\';',
-        'static-host': 'app_main.staticHost = \'' + staticHost + '\';'
-    };
-
     return gulp.src('./tours/index.html')
-        .pipe(htmlreplace(confReplace))
+        .pipe(htmlreplace(getConfReplace()))
         .pipe(gulp.dest(conf.publish + '/tours'));
 });
 
 
-gulp.task('html-replace', ['replace-index', 'release-tours']);
+gulp.task('html-replace', ['replace-index', 'release-tours', 'replace-browser']);
