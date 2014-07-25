6d1660e200690a71e7913d80e016eb0f13392313
Dmitry Kuznetsov <batmandarkside@gmail.com>
2014-07-25 14:51:54 +0400

    привожу билеты к нормальному виду

--- spa/js/angular/components/ticket/ticket-item.js
+++ spa/js/angular/components/ticket/ticket-item.js
@@ -3,9 +3,11 @@ angular.module('innaApp.conponents').
         'EventManager',
         'innaApp.API.events',
         '$filter',
+        '$routeParams',
+        'aviaHelper',
         '$templateCache',
         'DynamicBlock',
-        function (EventManager, Events, $filter, $templateCache, DynamicBlock) {
+        function (EventManager, Events, $filter, $routeParams, aviaHelper, $templateCache, DynamicBlock) {
 
             /**
              * Компонент TicketItem
@@ -16,15 +18,19 @@ angular.module('innaApp.conponents').
                 template: $templateCache.get('components/dynamic-block/templ/base.hbs.html'),
                 append: true,
                 data: {
-                    settings : {
-                        height : 200,
+                    settings: {
+                        height: 200,
                         countColumn: 2,
-                        classBlock : 'b-result_col_two_short b-result_flight-info'
-                    }
+                        classBlock: 'b-result_col_two_short b-result_flight-info'
+                    },
+                    showWarning : function(){
+                        return this.showWarning;
+                    },
+                    airLogo : aviaHelper.setEtapsTransporterCodeUrl
                 },
-                partials : {
-                    //collOneContent : see DynamicBlock
-                    collTwoContent : $templateCache.get('components/dynamic-block/templ/combination-price.hbs.html')
+                partials: {
+                    collOneContent: $templateCache.get('components/dynamic-block/templ/avia-dp.hbs.html'),
+                    collTwoContent: $templateCache.get('components/dynamic-block/templ/combination-price.hbs.html')
                 },
 
 
@@ -37,15 +43,21 @@ angular.module('innaApp.conponents').
                     virtualBundle.ticket = modelTicket;
                     virtualBundle.hotel = this.get('combinationModel').hotel;
 
+                    console.log(modelTicket.collectAirlines().etap);
+
                     this.set({
-                        virtualBundle : virtualBundle,
-                        modelTicket : modelTicket
+                        virtualBundle: virtualBundle,
+                        modelTicket: modelTicket
                     });
 
+
+
+                    //console.log(this.get('ticket'));
+
                     this.on({
-                        setCurrent : this.setCurrent,
-                        getTicketDetails : this.getHotelDetails,
-                        change : function(data){
+                        setCurrent: this.setCurrent,
+                        getTicketDetails: this.getHotelDetails,
+                        change: function (data) {
 
                         },
                         teardown: function (evt) {
@@ -54,17 +66,38 @@ angular.module('innaApp.conponents').
                     });
                 },
 
-                getTicketDetails : function(){
+                getTicketDetails: function () {
                     EventManager.fire(Events.DYNAMIC_SERP_TICKET_DETAILED_REQUESTED, this.get('modelTicket'));
                 },
 
-                setCurrent : function(){
+                setCurrent: function () {
                     EventManager.fire(Events.DYNAMIC_SERP_CHOOSE_TICKET, this.get('modelTicket'));
                 },
 
+                showWarning: function () {
+                    console.log('showWarning showWarning showWarning');
+                    var n = parseInt(this.get('NumSeats'));
+                    var routParam = angular.copy($routeParams);
+                    var passengerCount = parseInt(routParam.Adult) + (routParam.ChildrenAges ? routParam.ChildrenAges.length : 0);
 
-                parse: function (end) {
+                    if(!n) return false;
 
+                    switch (passengerCount) {
+                        case 1:
+                            return (n < 4);
+                        case 2:
+                            return (n < 7);
+                        default:
+                            return (n < 10);
+                    }
+
+                    return false;
+                },
+
+                parse: function (data) {
+
+
+                    return data;
                 }
 
             });
