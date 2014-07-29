3f86993c24c248c5e0a2a58a79ef4bb32f5c3a42
Dmitry Kuznetsov <batmandarkside@gmail.com>
2014-07-29 12:16:13 +0400

    закончил делать логику взаимодействия фильтрации и сортировки

--- spa/js/angular/components/list-panel/list-panel.js
+++ spa/js/angular/components/list-panel/list-panel.js
@@ -28,18 +28,18 @@ angular.module('innaApp.conponents').
             var ListPanel = Ractive.extend({
                 template: $templateCache.get('components/list-panel/templ/list.hbs.html'),
                 data: {
-                    iterable_hotels : false,
-                    iterable_tickets : false,
-                    EnumerableClone : [],
+                    iterable_hotels: false,
+                    iterable_tickets: false,
+                    EnumerableClone: [],
                     EnumerableList: [],
                     countItemsVisible: 10
                 },
-                partials : {
-                    EnumerableItemHotels : $templateCache.get('components/list-panel/templ/enumerableItemHotel.hbs.html'),
-                    EnumerableItemTickets : $templateCache.get('components/list-panel/templ/enumerableItemTicket.hbs.html')
+                partials: {
+                    EnumerableItemHotels: $templateCache.get('components/list-panel/templ/enumerableItemHotel.hbs.html'),
+                    EnumerableItemTickets: $templateCache.get('components/list-panel/templ/enumerableItemTicket.hbs.html')
                 },
                 components: {
-                    IndicatorFilters : IndicatorFilters,
+                    IndicatorFilters: IndicatorFilters,
                     HotelItem: HotelItem,
                     TicketItem: TicketItem
                 },
@@ -48,8 +48,8 @@ angular.module('innaApp.conponents').
                     this.enumerableClone = [];
                     this._filterTimeout = null;
 
-                    if(this.get('iterable_hotels')){
-                        this.parse(this.get('Enumerable'), { hotel :  true });
+                    if (this.get('iterable_hotels')) {
+                        this.parse(this.get('Enumerable'), { hotel: true });
                     }
 
 
@@ -87,7 +87,7 @@ angular.module('innaApp.conponents').
                     this.observe('Enumerable', function (newValue, oldValue, keypath) {
                         if (newValue) {
                             this.cloneData();
-                            this.set({waitData : false})
+                            this.set({waitData: false})
                         }
                     });
 
@@ -100,19 +100,19 @@ angular.module('innaApp.conponents').
 
                             /*console.log(newValue, 'newValue');
 
-                            console.table([
-                                {
-                                    newValue: newValue.length,
-                                    EnumerableList: this.get('EnumerableList').length,
-                                    Enumerable: this.get('Enumerable').length,
-                                    enumerableClone: this.enumerableClone.length
-                                }
-                            ]);*/
+                             console.table([
+                             {
+                             newValue: newValue.length,
+                             EnumerableList: this.get('EnumerableList').length,
+                             Enumerable: this.get('Enumerable').length,
+                             enumerableClone: this.enumerableClone.length
+                             }
+                             ]);*/
 
                             // после добавления элементов в EnumerableList
                             // обновляем координаты
                             // оборачиваем в setTimeout, так как нужно дождаться вставки элементов в DOM
-                            if(newValue.length) {
+                            if (newValue.length) {
                                 if (newValue.length != this.get('Enumerable').length) {
                                     setTimeout(this.updateCoords.bind(this), 0);
                                 } else {
@@ -138,17 +138,15 @@ angular.module('innaApp.conponents').
                     // выполняем фильтрацию не чаще 300ms
                     // защита от слишком частого нажатия на кнопки фильтрации
                     EventManager.on(Events.FILTER_PANEL_CHANGE, function (data) {
-                        console.log('change .FILTER_PANEL_CHANGE');
                         clearTimeout(that._filterTimeout);
-                        that._filterTimeout = setTimeout(function(){
+                        that._filterTimeout = setTimeout(function () {
                             that.doFilter(that.get('Enumerable'), data);
                         }, 300);
                     });
 
                     EventManager.on(Events.FILTER_PANEL_RESET, function (data) {
-                        console.log('Events.FILTER_PANEL_RESET');
                         clearTimeout(that._filterTimeout);
-                        that._filterTimeout = setTimeout(function(){
+                        that._filterTimeout = setTimeout(function () {
                             that.resetFilter();
                         }, 100);
                     });
@@ -221,11 +219,10 @@ angular.module('innaApp.conponents').
                         end = that.get('countItemsVisible'),
                         newDose = that.enumerableClone.splice(start, end);
 
-                    console.time("Execution time render");
-                    if (newDose.length) {
+                    //console.time("Execution time render");
+                    if (newDose.length)
                         this.set({EnumerableList: this.get('EnumerableList').concat(newDose)});
-                    }
-                    console.timeEnd("Execution time render");
+                    //console.timeEnd("Execution time render");
                 },
 
                 /**
@@ -239,7 +236,7 @@ angular.module('innaApp.conponents').
                 parse: function (data, opt_param) {
                     var that = this;
 
-                    if(opt_param.hotel) {
+                    if (opt_param.hotel) {
                         data.forEach(function (item) {
                             var modelHotel = new inna.Models.Hotels.Hotel(item)
                             var virtualBundle = new inna.Models.Dynamic.Combination();
@@ -268,50 +265,71 @@ angular.module('innaApp.conponents').
                  * @return {Array} новый набор
                  */
 
-                doFilter : function(collection, param_filters){
+                doFilter: function (collection, param_filters) {
                     var that = this;
                     var filterEnumerable = [];
 
                     // проход по коллекции данных
-                    console.time("Execution time took");
                     for (var j = 0; j < collection.length; j++) {
                         var item = collection[j];
 
                         // проход по фильтрам
-                        var filterResult = param_filters.filter(function(filters){
+                        var filterResult = param_filters.filter(function (filters) {
                             if (item[filters.name] == undefined) return false;
 
-                            if(filters.fn != undefined) {
+                            if (filters.fn != undefined)
                                 return filters.fn(item[filters.name]);
-                            }
+
                         });
 
 
                         //
-                        if(filterResult.length == param_filters.length) {
+                        if (filterResult.length == param_filters.length)
                             filterEnumerable.push(item)
-                        }
-                    }
-                    console.timeEnd("Execution time took");
 
-                    // ставим фильтр в конец очереди чтоб не блокировать
-                    // например переключение самих фильтров
-                    setTimeout(function () {
-                        that.cloneData(filterEnumerable);
-                    }, 0);
+                    }
 
                     // подписываемся на событие скролла если еще нет этого события
-                    if (!this.get('scroll') && filterEnumerable.length > 2) {
+                    if (!this.get('scroll') && filterEnumerable.length > 2)
                         this.addScroll();
-                    }
+
 
                     // если список меньше колличества разовой порции, то скролл нам не нужен
                     if (filterEnumerable.length <= this.get('countItemsVisible')) this.removeScroll();
 
-                    console.log(filterEnumerable, filterEnumerable.length, 'filterEnumerable');
+                    this.insertAfterFiltered(filterEnumerable);
+
+
+                    //console.log(filterEnumerable, filterEnumerable.length, 'filterEnumerable');
 
 
                     //EventManager.fire(Events.FILTER_PANEL_CLOSE_FILTERS);
+                },
+
+
+                /**
+                 * ставим фильтр в конец очереди чтоб не блокировать
+                 * например переключение самих фильтров
+                 * если есть свойство sortValue, то сортируем после фильтрации
+                 */
+                insertAfterFiltered: function (filteredData) {
+                    var that = this;
+
+                    this.set({
+                        filtered: true,
+                        EnumerableFiltered: filteredData
+                    })
+
+                    setTimeout(function () {
+                        var result = null;
+
+                        if (that.get('sortValue'))
+                            result = that.sorting(that.get('sortValue'), filteredData)
+                        else
+                            result = filteredData;
+
+                        that.cloneData(result);
+                    }, 0);
                 },
 
 
@@ -321,15 +339,22 @@ angular.module('innaApp.conponents').
                  * @param {String} sortValue
                  * @param {List<>} opt_sort_data
                  */
-
-                sorting : function(sortValue, opt_sort_data){
+                sorting: function (sortValue, opt_sort_data) {
                     var that = this;
-                    var sortData = opt_sort_data || this.get('Enumerable');
+                    var sortData = null;
+
+                    // Если когда то была фильтрация, то берем и сортируем именно отфильтрованный набор
+                    if (this.get('filtered') && this.get('EnumerableFiltered').length)
+                        sortData = this.get('EnumerableFiltered');
+                    else
+                        sortData = opt_sort_data || this.get('Enumerable');
+
+
                     var sortReverse = true;
                     var expression = sortValue;
 
 
-                    switch(sortValue){
+                    switch (sortValue) {
                         case 'PackagePrice':
                             sortReverse = false;
                     }
@@ -341,12 +366,21 @@ angular.module('innaApp.conponents').
 
                 /**
                  * Сбрасываем список до начального состояния без фильтров
+                 * Если установлено свойство sortValue, то сортируем набор
                  */
                 resetFilter: function () {
-                    if (!this.get('scroll')) {
-                        this.addScroll();
-                    }
-                    this.cloneData();
+                    var sortResult = null;
+                    this.set({
+                        filtered: false,
+                        EnumerableFiltered: []
+                    })
+
+                    if (!this.get('scroll')) this.addScroll();
+
+                    if (this.get('sortValue'))
+                        sortResult = this.sorting(this.get('sortValue'), null);
+
+                    this.cloneData(sortResult);
                 },
 
                 /**
@@ -354,16 +388,16 @@ angular.module('innaApp.conponents').
                  * @param opt_data
                  */
                 cloneData: function (opt_data) {
-                    if(opt_data) this.set('EnumerableList', []);
+                    if (opt_data) this.set('EnumerableList', []);
                     this.enumerableClone = [].concat(opt_data || this.get('Enumerable'));
+
                     // получаем первую порцию из n item
                     // далее по скроллингу
-
                     this.nextArrayDoseItems();
                 },
 
-                wait : function(){
-                    this.set({waitData : true})
+                wait: function () {
+                    this.set({waitData: true})
                 },
 
                 beforeInit: function (options) {
