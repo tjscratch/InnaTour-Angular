40530b0ccb14e9fd76eb534f4170dbde50f4edbb
Dmitry Kuznetsov <batmandarkside@gmail.com>
2014-08-01 19:48:14 +0400

    сделал вывод списка примененных фильтров на 80%

--- spa/js/angular/components/indicator-filters/indicator-filters.js
+++ spa/js/angular/components/indicator-filters/indicator-filters.js
@@ -3,18 +3,32 @@ innaAppConponents.
         'EventManager',
         'innaApp.API.events',
         '$templateCache',
-        function (EventManager, Events, $templateCache) {
+
+        // components
+        'Tripadvisor',
+        'Stars',
+        function (EventManager, Events, $templateCache, Tripadvisor, Stars) {
 
             var IndicatorFilters = Ractive.extend({
                 template: $templateCache.get('components/indicator-filters/templ/indicator-filters.hbs.html'),
-                data : {
-                    filters : [],
-                    mod_wrapper : false,
-                    atLeastOne : false
+                data: {
+                    filters: [],
+                    isArray : angular.isArray,
+                    atLeastOne: false
+                },
+                components: {
+                    Tripadvisor: Tripadvisor,
+                    Stars: Stars
                 },
                 init: function (options) {
+                    var that = this;
+
+
                     this.on({
-                        action : this.action
+                        action: this.action,
+                        removeFilter : function(data){
+                            console.log(data.context);
+                        }
                     })
 
 
@@ -24,10 +38,19 @@ innaAppConponents.
                                 this.set({location: document.location});
                                 $(this._input).select();
                             }
-                        },{defer: true});
+                        }, {defer: true});
+
+                    EventManager.on(Events.FILTER_PANEL_CHANGE, function (data) {
+                        clearTimeout(that._filterTimeout);
+                        that._filterTimeout = setTimeout(function () {
+                            that.set('filters', data);
+                            console.log(data, 'IndicatorFilters');
+                        }, 300);
+                    });
                 },
 
-                action : function(){
+
+                action: function () {
                     // событие переключения на карты и обратно
                     EventManager.fire(Events.DYNAMIC_SERP_TOGGLE_MAP);
                 }
