dbe9838eee9448ee6579f77185e88158aeae4b84
Sveshnikov Alexander <devnull@localhost>
2014-08-01 14:07:39 +0400

    fix config for test

--- spa/js/angular/config.js
+++ spa/js/angular/config.js
@@ -1,11 +1,11 @@
 var app_main = {};
-app_main.host = 'https://api.inna.ru';
+app_main.host = 'http://api.test.inna.ru';
 
-app_main.b2bHost = 'https://b2b.inna.ru';
+app_main.b2bHost = 'http://b2b.test.inna.ru';
 
-app_main.frontHost = 'https://inna.ru';
+app_main.frontHost = 'http://test.inna.ru';
 
-app_main.staticHost = 'https://s.inna.ru';
+app_main.staticHost = 'http://s.test.inna.ru';
 
 app_main.tripadvisor = 'https://www.tripadvisor.ru/WidgetEmbed-cdspropertydetail?display=true&partnerId=32CB556934404C699237CD7F267CF5CE&lang=ru&locationId=';
 
