a15d9439eb7b15db473e6a381a5b57e27d91799c
Sveshnikov Alexander <devnull@localhost>
2014-08-01 14:14:03 +0400

    config fix

--- spa/js/angular/config.js
+++ spa/js/angular/config.js
@@ -1,13 +1,24 @@
 var app_main = {};
-app_main.host = 'http://api.test.inna.ru';
 
+//<!-- build:app-host -->
+app_main.host = "http://api.test.inna.ru";
+//<!-- endbuild -->
+
+//<!-- build:b2b-host -->
 app_main.b2bHost = 'http://b2b.test.inna.ru';
+//<!-- endbuild -->
 
+//<!-- build:front-host -->
 app_main.frontHost = 'http://test.inna.ru';
+//<!-- endbuild -->
 
+//<!-- build:static-host -->
 app_main.staticHost = 'http://s.test.inna.ru';
+//<!-- endbuild -->
 
-app_main.tripadvisor = 'https://www.tripadvisor.ru/WidgetEmbed-cdspropertydetail?display=true&partnerId=32CB556934404C699237CD7F267CF5CE&lang=ru&locationId=';
+//<!-- build:tripadvisor -->
+app_main.tripadvisor = 'http://www.tripadvisor.ru/WidgetEmbed-cdspropertydetail?display=true&partnerId=32CB556934404C699237CD7F267CF5CE&lang=ru&locationId=';
+//<!-- endbuild -->
 
 app_main.constants = {};
 app_main.constants.offersCategoriesProgramm = '1';
