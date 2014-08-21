f7649e1582cb5e0025ee03af5b586fe9487e7a95
Greg Karelin <grundiss@gmail.com>
2014-08-21 17:48:30 +0400

    https://innatec.atlassian.net/browse/IN-2600 | Когда нет номеров, выводить popup  "К сожалению, свободных номеров в данный момент нет"

--- spa/js/angular/pages/page-dynamic-details/page-dynamic-details.js
+++ spa/js/angular/pages/page-dynamic-details/page-dynamic-details.js
@@ -48,7 +48,6 @@ innaAppControllers
                 };
             }
 
-
             /*Private*/
             var _tripadvisor = null;
             var _stars = null;
@@ -208,8 +207,12 @@ innaAppControllers
                     Rooms : true,
 
                     success: function (data) {
-                        $scope.hotelRooms = data.Rooms;
-                        onload();
+                        if(data.Rooms.length) {
+                            $scope.hotelRooms = data.Rooms;
+                            onload();
+                        } else {
+                            $scope.baloon.showErr("К сожалению, свободных номеров в данный момент нет");
+                        }
                     },
                     error: function () {
                         console.log('no rooms');
