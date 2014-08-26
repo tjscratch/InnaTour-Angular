e7effd62f1d68d7177819ef2cd36fe51b5d921b8
Sveshnikov Alexander <devnull@localhost>
2014-08-26 15:25:09 +0400

    fix

--- spa/js/angular/partners.js
+++ spa/js/angular/partners.js
@@ -3,6 +3,10 @@
         'biletix': '/biletix/biletix.base.css'
     };
 
-    var src = partnersMap[patrner];
+    //var src = partnersMap[patrner];
+    //console.log(app.host);
+    //console.log(location.host);
+
+    //document.getElementById("myList").insertBefore(newItem, existingItem);
 
 }());
