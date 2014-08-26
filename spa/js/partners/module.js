1d199618865b089fe0806735eea586b00f8ee7fa
Sveshnikov Alexander <devnull@localhost>
2014-08-26 18:50:20 +0400

    тесты по фреймам с формой

---
+++ spa/js/partners/module.js
@@ -0,0 +1,41 @@
+﻿window.parent.inna = {
+    searchFrom: {
+        init: function () {
+            //alert('inna.searchFrom init');
+            var fr = document.createElement("IFRAME");
+            fr.id = "innaFrame1"
+            fr.onload = autoResize(fr.id);
+            fr.style.width = 1200 + "px";
+            fr.style.height = "500px";
+            //fr.style.overflow = "hidden";
+            fr.border = 0;
+            fr.frameBorder = 0;
+            fr.src = "http://lh.inna.ru/";
+            document.getElementById("inna-frame").appendChild(fr);
+            //document.body.appendChild(fr);
+
+            function autoResize(id) {
+                setTimeout(function () {
+                    resize(id);
+                }, 0);
+                //resize(id);
+            }
+
+            function resize(id) {
+                var newheight;
+                var newwidth;
+
+                if (document.getElementById) {
+                    var frame = document.getElementById(id);
+                    newheight = frame.contentWindow.document.body.scrollHeight;
+                    //newwidth = document.getElementById(id).contentWindow.document.body.scrollWidth;
+                }
+
+                console.log('newheight', newheight);
+                frame.height = "";
+                frame.height = (newheight) + "px";
+                //document.getElementById(id).width = (newwidth) + "px";
+            }
+        }
+    }
+};
