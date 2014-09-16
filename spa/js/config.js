90619968e79dd6bcefa58922317f8ca4223b854b
Dmitry Kuznetsov <batmandarkside@gmail.com>
2014-09-16 15:56:51 +0400

    изменил сборку проекта

--- spa/js/config.js
+++ spa/js/config.js
@@ -1,24 +1,8 @@
 var app_main = {};
-
-//<!-- build:api-host -->
-app_main.apiHost = "http://test.inna.ru"; //'https://api.inna.ru';
-//<!-- endbuild -->
-
-//<!-- build:b2b-host -->
-app_main.b2bHost = 'http://b2b.test.inna.ru';
-//<!-- endbuild -->
-
-//<!-- build:front-host -->
-app_main.frontHost = 'http://test.inna.ru';
-//<!-- endbuild -->
-
-//<!-- build:static-host -->
-app_main.staticHost = 'http://s.test.inna.ru';
-//<!-- endbuild -->
-
-//<!-- build:tripadvisor -->
-app_main.tripadvisor = 'http://www.tripadvisor.ru/WidgetEmbed-cdspropertydetail?display=true&partnerId=32CB556934404C699237CD7F267CF5CE&lang=ru&locationId=';
-//<!-- endbuild -->
-
+app_main.apiHost = "@@api_host"; //http://test.inna.ru
+app_main.b2bHost = "@@b2b_host"; //'http://b2b.test.inna.ru';
+app_main.frontHost = "@@front_host"; //'http://test.inna.ru';
+app_main.staticHost = "@@static_host";  //'http://s.test.inna.ru';
+app_main.tripadvisor = "@@tripadvisor"; //'http://www.tripadvisor.ru/WidgetEmbed-cdspropertydetail?display=true&partnerId=32CB556934404C699237CD7F267CF5CE&lang=ru&locationId=';
 app_main.constants = {};
 app_main.constants.offersCategoriesProgramm = '1';
