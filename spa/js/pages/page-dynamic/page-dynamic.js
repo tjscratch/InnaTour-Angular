5d1f0216b40025495f6e0916cce7d7f6720a5fa8
Stolbukhin Maksim <maxstbn@gmail.com>
2014-10-06 18:22:20 +0400

    Backout changeset 8db7ed287903032814754581e6c2e01851f01015

--- spa/js/pages/page-dynamic/page-dynamic.js
+++ spa/js/pages/page-dynamic/page-dynamic.js
@@ -61,6 +61,56 @@ innaAppControllers
             $scope.filtersSettingsHotels = null;
             $scope.filtersSettingsTicket = null;
 
+
+            /**
+             * кнопка прокрутки страницы наверх
+             */
+            $scope.goToTop = function () {
+                $location.hash('top');
+                $anchorScroll();
+            };
+
+            function showGoToTop() {
+                $scope.safeApply(function () {
+                    $scope.goToTopShow = true;
+                })
+            }
+
+            function hideGoToTop() {
+                $scope.safeApply(function () {
+                    $scope.goToTopShow = false;
+                })
+            }
+
+            function GoToTopBtn() {
+                var footerTop = utils.getCoords(footerEl).top;
+                var windowHeight = utils.getScrollTop() + window.innerHeight;
+                if (windowHeight > 900) {
+                    showGoToTop();
+                } else {
+                    hideGoToTop();
+                }
+                var wr = windowHeight - footerTop;
+                if (wr > 5) {
+                    $scope.safeApply(function () {
+                        $scope.goToTopStyle = {
+                            'bottom': wr + 'px',
+                            'transition': 'bottom 0s'
+                        };
+                    })
+                } else {
+                    $scope.safeApply(function () {
+                        $scope.goToTopStyle = {
+                            'bottom': '0px',
+                            'transition': 'bottom .15s'
+                        };
+                    })
+                }
+            }
+
+            var footerEl = document.querySelector(".footer");
+            document.addEventListener('scroll', GoToTopBtn);
+
             //кнопка нового поиска для WL
             function setWlModel(data) {
                 $scope.WlNewSearchModel = new inna.Models.WlNewSearch({
@@ -254,6 +304,7 @@ innaAppControllers
                                     }
                                 });
 
+                                setTimeout(GoToTopBtn, 0);
                                 this.set('updateHotel', true);
                             }
                         },
@@ -658,6 +709,7 @@ innaAppControllers
                 }
 
                 $(document).off('scroll');
+                $(document).off('scroll', GoToTopBtn);
             })
         }
     ]);
