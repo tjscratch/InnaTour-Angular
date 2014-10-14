c1c52b5d313e0556b6957a4b7a7f53cafa919143
Dmitry Kuznetsov <batmandarkside@gmail.com>
2014-10-14 17:26:09 +0400

    test

--- spa/js/filters.js
+++ spa/js/filters.js
@@ -174,3 +174,12 @@ innaAppFilters.filter('console', function(){
         return '';
     };
 });
+
+
+innaAppFilters.filter('partition', function() {
+    var part = function(arr, size) {
+        if ( 0 === arr.length ) return [];
+        return [ arr.slice( 0, size ) ].concat( part( arr.slice( size ), size) );
+    };
+    return part;
+});
