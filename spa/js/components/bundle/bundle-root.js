1046fc98bfd21af88fa08a2c36753a99f60053ef
Dmitry Kuznetsov <batmandarkside@gmail.com>
2014-10-23 17:40:32 +0400

    доделали переделку ценообразования в ДП

--- spa/js/components/bundle/bundle-root.js
+++ spa/js/components/bundle/bundle-root.js
@@ -163,6 +163,11 @@ angular.module('innaApp.directives')
 
                         EventManager.on(Events.DYNAMIC_SERP_CHOOSE_TICKET, function (data) {
                             $scope.safeApply(function () {
+
+                                // TODO : заменяем дату заезда  в отель
+                                // так как при выборе другого авиа билета может измениться дата прилета
+                                $scope.combination.hotel.data.CheckIn = $scope.combination.ticket.data.ArrivalDate;
+
                                 $scope.isChooseHotel = true;
                                 $scope.display.fullDisplay();
                             });
