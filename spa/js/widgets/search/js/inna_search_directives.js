431cb877a830bb07857f9d1fa8a9304a8214b8f4
Maxim Stolbukhin <maxstbn@gmail.com>
2014-11-05 19:05:45 +0300

    config stylus

--- spa/js/widgets/search/js/inna_search_directives.js
+++ spa/js/widgets/search/js/inna_search_directives.js
@@ -7,8 +7,22 @@
     function searchForm() {
         return{
             restrict: 'E',
-            templateUrl: 'tpl/form.html',
-            controller: function () {
+            templateUrl: '/inna-frontend/spa/js/widgets/search/templ/form.html',
+            controller: function ($scope, $http) {
+
+                $scope.getLocation = function (val) {
+                    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+                        params: {
+                            address: val,
+                            sensor: false
+                        }
+                    }).then(function (response) {
+                        return response.data.results.map(function (item) {
+                            return item.formatted_address;
+                        });
+                    });
+                };
+                
 
             }
         }
