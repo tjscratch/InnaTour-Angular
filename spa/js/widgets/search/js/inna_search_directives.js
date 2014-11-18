729f574ef26bbde64c9a397349d275a9968d3804
Maxim Stolbukhin <maxstbn@gmail.com>
2014-11-18 16:52:02 +0300

    bug fix typeahead

--- spa/js/widgets/search/js/inna_search_directives.js
+++ spa/js/widgets/search/js/inna_search_directives.js
@@ -24,6 +24,32 @@
                         $scope.locationFrom = {id: response.Id, name: fullName, iata: response.CodeIata};
                     });
 
+                    /**
+                     * https://inna.ru/api/v1/Dictionary/Directory
+                     */
+                    $scope.getLocationFrom = function (val) {
+                        return $http.get('https://inna.ru/api/v1/Dictionary/Directory', {
+                            params: {
+                                term: val.split(',')[0].trim()
+                            }
+                        }).then(function (response) {
+                            var data = []
+                            angular.forEach(response.data, function (item) {
+                                var fullName = item.Name + ", " + item.CountryName;
+                                var fullNameHtml = "<span class='i-name'>" + item.Name + "</span>," + "<span class='i-country'>" + item.CountryName + "</span>";
+                                data.push({id: item.Id, nameHtml: fullNameHtml, name: fullName, iata: item.CodeIata});
+                                if(item.Airport){
+                                    angular.forEach(item.Airport, function (item) {
+                                        var fullName = item.Name + ", " + item.CountryName;
+                                        var fullNameHtml = "<span class='i-name i-name-airport'>" + item.Name + "</span>";
+                                        data.push({id: item.Id, nameHtml: fullNameHtml, name: fullName, iata: item.CodeIata});
+                                    });
+                                }
+                            });
+                            return data;
+                        });
+                    };
+                    
 
                     /**
                      * автокомплит выбора локации
@@ -33,7 +59,7 @@
                     $scope.getLocation = function (val) {
                         return $http.get('https://inna.ru/api/v1/Dictionary/Hotel', {
                             params: {
-                                term: val
+                                term: val.split(',')[0].trim()
                             }
                         }).then(function (response) {
                             var data = []
