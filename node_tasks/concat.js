6ba5cf59135e47bda81dab66bd39f8ccda2bd7e2
Maxim Stolbukhin <maxstbn@gmail.com>
2014-11-26 16:20:28 +0300

    сделал загрузку библиотек с нашего хоста

--- node_tasks/concat.js
+++ node_tasks/concat.js
@@ -38,15 +38,20 @@ gulp.task('build-concat', [
 
 gulp.task('concat-lib', function () {
     return gulp.src([
+            conf.dest + '/lib/angular/1.3.4/angular.min.js',
+            conf.dest + '/lib/angular/1.3.4/angular-route.min.js',
+            conf.dest + '/lib/angular/1.3.4/angular-sanitize.min.js',
+            conf.dest + '/lib/angular/angular-cookies.min.js',
+            conf.dest + '/lib/angular/angular-locale_ru-ru.js',
+        
             conf.dest + '/lib/moment.js',
             conf.dest + '/lib/moment-ru.js',
             conf.dest + '/lib/underscore.js',
             conf.dest + '/lib/ractive/ractive.0.6.1.js',
             conf.dest + '/lib/ractive/ractive-events-hover.js',
             conf.dest + '/lib/polyfill/**/*.js',
+            conf.dest + '/lib/jquery/jquery-2.1.1.min.js',
             conf.dest + '/lib/jquery.maskedinput.js',
-            conf.dest + '/lib/angular/angular-cookies.min.js',
-            conf.dest + '/lib/angular/angular-locale_ru-ru.js',
             conf.dest + '/lib/google.maps.clustering.js',
             conf.dest + '/lib/jquery.ui.datepicker-ru.js',
             conf.dest + '/lib/datepicker/datepicker.js',
