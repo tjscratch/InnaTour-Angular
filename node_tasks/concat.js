7fc9725c7990cadba79020d81eb8a9f266150966
batmandarkside <batmandarkside@gmail.com>
2014-12-09 00:45:31 +0300

    временно переименовал папку

--- node_tasks/concat.js
+++ node_tasks/concat.js
@@ -51,6 +51,7 @@ gulp.task('concat-bower-components', function () {
         conf.bower + '/angular-sanitize/angular-sanitize.min.js',
         conf.bower + '/angular-route/angular-route.min.js',
         conf.bower + '/angular-hotkeys/build/hotkeys.min.js',
+        conf.bower + '/angular-hotkeys-temp/build/hotkeys.min.js',
         conf.bower + '/moment/min/moment.min.js',
         conf.bower + '/moment/locale/ru.js'
     ], {base: 'bower_components'})
