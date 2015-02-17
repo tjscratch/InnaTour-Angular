ffc9d948b72727f4b384f9d7b6047e641bdae05a
Maxim Stolbukhin <maxstbn@gmail.com>
2015-02-17 14:34:01 +0300

    progress

--- spa/js/components/agency-reg-form/agency-reg-form.js
+++ spa/js/components/agency-reg-form/agency-reg-form.js
@@ -1,18 +1,21 @@
 innaAppConponents.controller("AgencyRegFormCtrl", function ($scope, dataService) {
 
-    $scope.partnerReg = {};
+    $scope.agencyReg = {};
 
 
-    $scope.partnerSubmit = function () {
-        console.log($scope.partnerReg)
-        dataService.agencyCreate($scope.partnerReg)
-            .success(function (data) {
-                console.log(data)
-            })
-            .error(function (data) {
-                console.log(data)
-            })
-        console.log($scope.partnerReg)
+    $scope.agencySubmit = function (form) {
+
+        console.log(form.$valid)
+
+        if (form.$valid) {
+            dataService.agencyCreate($scope.agencyReg)
+                .success(function (data) {
+                    console.log(data)
+                })
+                .error(function (data) {
+                    console.log(data)
+                })
+        }
     }
 
 
