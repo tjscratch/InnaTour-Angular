5be03556fa3e7e1354ef8307c75d020dc75e3d34
Stolbukhin Maksim <maxstbn@gmail.com>
2015-02-18 22:59:23 +0300

    верстка + js

--- spa/js/components/agency-reg-form/agency-reg-form.js
+++ spa/js/components/agency-reg-form/agency-reg-form.js
@@ -1,7 +1,22 @@
-innaAppConponents.controller("AgencyRegFormCtrl", function ($scope, dataService) {
+innaAppConponents.controller("AgencyRegFormCtrl", function ($rootScope, $scope, dataService) {
 
+	$rootScope.BodyStyleGlobal = {
+		'background-color': '#fff'
+	}
+	
     $scope.agencyReg = {};
 
+	$scope.adressChecked = false;
+	$scope.$watch('adressChecked', function(data){
+		if(data){
+			$scope.agencyReg.PostIndex = $scope.agencyReg.RegistredAddressIndex;
+			$scope.agencyReg.Address = $scope.agencyReg.RegistredAddress;
+		}else{
+			$scope.agencyReg.PostIndex = '';
+			$scope.agencyReg.Address = '';
+		}
+	})
+	
 
     $scope.agencySubmit = function (form) {
 
