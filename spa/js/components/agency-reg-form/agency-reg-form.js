45154dbd9ec3320e152368061b4b2b2cc6cefd66
Maxim Stolbukhin <maxstbn@gmail.com>
2015-02-19 13:31:15 +0300

    progress

--- spa/js/components/agency-reg-form/agency-reg-form.js
+++ spa/js/components/agency-reg-form/agency-reg-form.js
@@ -1,22 +1,34 @@
 innaAppConponents.controller("AgencyRegFormCtrl", function ($rootScope, $scope, dataService) {
 
-	$rootScope.BodyStyleGlobal = {
-		'background-color': '#fff'
-	}
-	
-    $scope.agencyReg = {};
+    $rootScope.BodyStyleGlobal = {
+        'background-color': '#fff'
+    }
 
-	$scope.adressChecked = false;
-	$scope.$watch('adressChecked', function(data){
-		if(data){
-			$scope.agencyReg.PostIndex = $scope.agencyReg.RegistredAddressIndex;
-			$scope.agencyReg.Address = $scope.agencyReg.RegistredAddress;
-		}else{
-			$scope.agencyReg.PostIndex = '';
-			$scope.agencyReg.Address = '';
-		}
-	})
-	
+    //$scope.agencyReg = {
+    //    Name: "Name",
+    //    INN: "INN",
+    //    KPP: "KPP",
+    //    BossName: "BossName",
+    //    Boss: "Boss",
+    //    RegistredAddress: "RegistredAddress",
+    //    RegistredAddressIndex: "RegistredAddressIndex",
+    //    Address: "Address",
+    //    PostIndex: "PostIndex",
+    //    Phones: "89099596787",
+    //    Site: "Site.ru",
+    //    Email: "email@google.com"
+    //};
+
+    $scope.adressChecked = true;
+    $scope.$watch('adressChecked', function (data) {
+        if (data) {
+            $scope.agencyReg.PostIndex = $scope.agencyReg.RegistredAddressIndex;
+            $scope.agencyReg.Address = $scope.agencyReg.RegistredAddress;
+        } else {
+            $scope.agencyReg.PostIndex = '';
+            $scope.agencyReg.Address = '';
+        }
+    })
 
     $scope.agencySubmit = function (form) {
 
@@ -25,10 +37,10 @@ innaAppConponents.controller("AgencyRegFormCtrl", function ($rootScope, $scope, 
         if (form.$valid) {
             dataService.agencyCreate($scope.agencyReg)
                 .success(function (data) {
-                    console.log(data)
+                    console.log('dataService-' + data)
                 })
                 .error(function (data) {
-                    console.log(data)
+                    console.log('dataService-' + data)
                 })
         }
     }
