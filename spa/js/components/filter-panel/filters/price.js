4d36ee73bb14e82092637d501c346bb3d23bc5f1
Stolbukhin Maksim <maxstbn@gmail.com>
2015-04-23 14:41:19 +0300

    сделал активным инпут в фильтре по цене в ДП

--- spa/js/components/filter-panel/filters/price.js
+++ spa/js/components/filter-panel/filters/price.js
@@ -1,152 +1,167 @@
 angular.module('innaApp.components').
-    factory('FilterPrice', [
-        'EventManager',
-        '$filter',
-        '$templateCache',
-        '$routeParams',
-        'innaAppApiEvents',
+	factory('FilterPrice', [
+		'EventManager',
+		'$filter',
+		'$templateCache',
+		'$routeParams',
+		'innaAppApiEvents',
 
-        'ClassFilter',
-        function (EventManager, $filter, $templateCache, $routeParams, Events, ClassFilter) {
+		'ClassFilter',
+		function (EventManager, $filter, $templateCache, $routeParams, Events, ClassFilter) {
 
-            var FilterPrice = ClassFilter.extend({
-                template: $templateCache.get('components/filter-panel/templ-filters/price.hbs.html'),
-                data: {
-                    setValue: 0,
-                    value: {
-                        name: 'PackagePrice',
-                        val: [],
-                        fn: function (data, component_val) {
-                            return (data <= component_val.val[0]);
-                        }
-                    }
-                },
-                components: {
+			var FilterPrice = ClassFilter.extend({
+				template  : $templateCache.get('components/filter-panel/templ-filters/price.hbs.html'),
+				data      : {
+					setValue: 0,
+					value   : {
+						name: 'PackagePrice',
+						val : [],
+						fn  : function (data, component_val) {
+							return (data <= component_val.val[0]);
+						}
+					}
+				},
+				components: {},
+				onrender  : function (options) {
+					this._super(options);
+					var that = this;
+					//this.FilterData.Value = $filter('price')(this.FilterData.Max);
+					this.SaveData = [];
+					this._timeOut = null;
+					this._slider = null;
 
-                },
-                onrender: function (options) {
-                    this._super(options);
-                    var that = this;
-                    this.SaveData = [];
-                    this._timeOut = null;
-                    this._slider = null;
+					this.set({'FilterData.Value': $filter('price')(this.get('FilterData.Max'))});
+					
+					this.on({
+						change        : function (data) {
+							if (data['FilterData.Value'] && data['setValue']) {
 
-                    this.on({
-                        change: function (data) {
-                            if (data['FilterData.Value'] && data['setValue']) {
+								var priceValue = parseInt(data['FilterData.Value'].replace(/\s+/g, ''), 10);
 
-                                var priceValue = parseInt(data['FilterData.Value'], 10);
+								clearTimeout(this._timeOut);
+								this._timeOut = setTimeout(function () {
 
-                                clearTimeout(this._timeOut);
-                                this._timeOut = setTimeout(function () {
-
-                                    if (priceValue > 0) {
-                                        this.set('value.val', [priceValue])
-                                        this.SaveData = [priceValue];
-                                    }
-                                    else {
-                                        this.set('value.val', []);
-                                        this.spliceSaveData();
-                                    }
-
-
-                                    // выставляем значение слайдера
-                                    this._slider.slider('value', priceValue);
-                                    this.fire('onCheckedFilter', {
-                                        name : this.get('value.name'),
-                                        value : this.get('value')
-                                    });
-                                    this.hasSelected();
-                                }.bind(this), 300);
-                            }
-                        },
-
-                        resetFilter: function () {
-                            this.set({'FilterData.Value': this.get('FilterData.Max')});
-
-                            if (this._slider) {
-                                this._slider.slider('value', this.get('FilterData.Max'));
-                            }
-                        },
-                        teardown: function (evt) {
-                            if (this._slider)
-                                this._slider.slider("destroy");
-
-                            this._slider = null;
-                        }
-                    });
-                },
+									if (priceValue > 0) {
+										this.set('value.val', [priceValue])
+										this.SaveData = [priceValue];
+									}
+									else {
+										this.set('value.val', []);
+										this.spliceSaveData();
+									}
 
 
-                mergeData: function () {
-                    var that = this;
+									// выставляем значение слайдера
+									this._slider.slider('value', priceValue);
+									this.fire('onCheckedFilter', {
+										name : this.get('value.name'),
+										value: this.get('value')
+									});
+									this.hasSelected();
+								}.bind(this), 300);
+							}
+						},
+						setFilterPrice: function (data) {
+							var val = data.context.FilterData.Value.replace(/\s+/g, '');
+							var minPrice = this.get('FilterData.Min');
+							val = parseInt(val, 10);
+							if (val >= minPrice) {
+								this.set({
+									'setValue'        : Math.random() * 1000,
+									'FilterData.Value': $filter('price')(val)
+								});
+							}
+						},
+						resetFilter   : function () {
+							//this.set({'FilterData.Value': this.get('FilterData.Max')});
+							this.set({'FilterData.Value': $filter('price')(this.get('FilterData.Max'))});
 
-                    if (this.SaveData.length) {
-                        this.slide(that.SaveData[0]);
-                        this.set('FilterData.Value', that.SaveData[0]);
-                    }
-                },
+							if (this._slider) {
+								this._slider.slider('value', this.get('FilterData.Max'));
+							}
+						},
+						teardown      : function (evt) {
+							if (this._slider) {
+								this._slider.slider("destroy");
+							}
 
-                spliceSaveData: function () {
-                    if (this.SaveData.length) this.SaveData = [];
-                },
-
-
-                /**
-                 *
-                 * @param data
-                 * @override
-                 */
-                IndicatorFiltersItemRemove: function (data) {
-                    //this._super(data);
-                    var that = this;
-
-                    this.set({
-                        'value.val': [],
-                        'FilterData.Value': that.get('FilterData.Max')
-                    });
-                    this.spliceSaveData();
-                    this._slider.slider('value', this.get('FilterData.Max'));
-
-                    this.fire('onCheckedFilter', {
-                        name : this.get('value.name'),
-                        value : this.get('value')
-                    });
-                    this.hasSelected();
-                },
+							this._slider = null;
+						}
+					});
+				},
 
 
-                /**
-                 * Срабатывает на изменение положени слайдера
-                 * свойство setValue выставляется для проверки что это настоящее изменение цены,
-                 * а не сброс фильтра
-                 * @param val
-                 */
-                slide: function (val) {
-                    this.set({
-                        'setValue': Math.random() * 1000,
-                        'FilterData.Value': val
-                    });
-                },
+				mergeData: function () {
+					var that = this;
 
-                oncomplete: function (data) {
-                    var that = this;
-                    var slider = this.find('.js-range');
+					if (this.SaveData.length) {
+						this.slide(that.SaveData[0]);
+						this.set('FilterData.Value', $filter('price')(that.SaveData[0]));
+					}
+				},
 
-                    this._slider = $(slider).slider({
-                        range: "min",
-                        animate: true,
-                        min: that.get('FilterData.Min'),
-                        max: that.get('FilterData.Max'),
-                        value: that.get('FilterData.Value'),
-                        slide: function (event, ui) {
-                            that.slide(ui.value)
-                        }
-                    });
-                }
-            });
+				spliceSaveData: function () {
+					if (this.SaveData.length) {
+						this.SaveData = [];
+					}
+				},
 
-            return FilterPrice;
-        }]);
+
+				/**
+				 *
+				 * @param data
+				 * @override
+				 */
+				IndicatorFiltersItemRemove: function (data) {
+					//this._super(data);
+					var that = this;
+
+					this.set({
+						'value.val'       : [],
+						'FilterData.Value': $filter('price')(that.get('FilterData.Max'))
+					});
+					this.spliceSaveData();
+					this._slider.slider('value', this.get('FilterData.Max'));
+
+					this.fire('onCheckedFilter', {
+						name : this.get('value.name'),
+						value: this.get('value')
+					});
+					this.hasSelected();
+				},
+
+
+				/**
+				 * Срабатывает на изменение положени слайдера
+				 * свойство setValue выставляется для проверки что это настоящее изменение цены,
+				 * а не сброс фильтра
+				 * @param val
+				 */
+				slide     : function (val) {
+					this.set({
+						'setValue'        : Math.random() * 1000,
+						'FilterData.Value': $filter('price')(val)
+					});
+				},
+				oncomplete: function (data) {
+					var that = this;
+					var slider = this.find('.js-range');
+
+					this._slider = $(slider).slider({
+						range  : "min",
+						animate: true,
+						min    : that.get('FilterData.Min'),
+						max    : that.get('FilterData.Max'),
+						value  : that.get('FilterData.Value'),
+						slide  : function (event, ui) {
+							that.slide(ui.value)
+						}
+					});
+				}
+			});
+
+			return FilterPrice;
+		}
+	]);
 
 
