c5a1544f05262d6b865541ae5216b958ff77563c
Stolbukhin Maksim <maxstbn@gmail.com>
2015-05-07 15:56:40 +0300

    форма авторизации для спутника

--- spa/js/controllers/auth/auth.js
+++ spa/js/controllers/auth/auth.js
@@ -1,285 +1,337 @@
 angular.module('innaApp.controllers')
-    .controller('AuthCtrl', [
-        '$scope',
-        '$location',
-        'aviaHelper',
-        'innaAppApiEvents',
-        'AuthDataProvider',
-        'innaApp.Urls',
-        'modelAuth',
-        '$route',
-        function($scope, $location, aviaHelper, Events, AuthDataProvider, app, modelAuth, $route){
-            /*Private*/
-            var setUserInfo = function (data, needInitLastUserAfterLoginCheck) {
-                if(data && data["Email"]) {
-                    Raven.setUserContext({
-                        email: data["Email"],
-                        id: data["Email"],
-                        data : data
-                    });
-                }
+	.controller('AuthCtrl', [
+		'$scope',
+		'$timeout',
+		'$location',
+		'aviaHelper',
+		'innaAppApiEvents',
+		'AuthDataProvider',
+		'innaApp.Urls',
+		'modelAuth',
+		'$route',
+		function ($scope, $timeout, $location, aviaHelper, Events, AuthDataProvider, app, modelAuth, $route) {
 
-                $scope.safeApply(function(){
-                    $scope.$root.user = new modelAuth(data);
+			/**
+			 * партнерка WL
+			 */
+			var partner = window.partners ? window.partners.getPartner() : null;
 
-                    if($scope.$root.user.isAgency() && !$scope.user.raw.AgencyActive) {
-                        $scope.logout();
-                    }
 
-                    //проверяем, нужно ли перезагрузить страницу
-                    if ($scope.reloadChecker) {
-                        if (needInitLastUserAfterLoginCheck) {//флаг, говорит, что нужно проинициализировать последнего пользователя
-                            $scope.reloadChecker.saveLastUser();
-                        }
-                        $scope.reloadChecker.checkReloadPage();
-                    }
-                });
-            }
+			/*Private*/
+			var setUserInfo = function (data, needInitLastUserAfterLoginCheck) {
+				if (data && data["Email"]) {
+					Raven.setUserContext({
+						email: data["Email"],
+						id   : data["Email"],
+						data : data
+					});
+				}
 
-            /*Methods*/
-            $scope.close = function(){
-                utils.scrollFix(true)
-                $scope.isLoginPopupOpened = false;
-                $scope.display = $scope.DISPLAY_SIGNIN;
-            };
+				$scope.safeApply(function () {
+					$scope.$root.user = new modelAuth(data);
 
-            $scope.logout = function (silent) {
-                var wasLoggedUser = $scope.$root.user;
+					if ($scope.$root.user.isAgency() && !$scope.user.raw.AgencyActive) {
+						$scope.logout();
+					}
 
-                $scope.$root.user = null;
+					//проверяем, нужно ли перезагрузить страницу
+					if ($scope.reloadChecker) {
+						if (needInitLastUserAfterLoginCheck) {//флаг, говорит, что нужно проинициализировать последнего пользователя
+							$scope.reloadChecker.saveLastUser();
+						}
+						$scope.reloadChecker.checkReloadPage();
+					}
+				});
+			}
 
-                if (!silent) {
-                    AuthDataProvider.logout(onLogoutCompleteOrError, onLogoutCompleteOrError);
+			/*Methods*/
+			$scope.close = function () {
+				utils.scrollFix(true)
+				$scope.isLoginPopupOpened = false;
+				$scope.display = $scope.DISPLAY_SIGNIN;
+			};
 
-                    function onLogoutCompleteOrError() {
-                        $scope.$emit(Events.AUTH_SIGN_OUT, wasLoggedUser);
-                    }
-                }
-            };
+			$scope.logout = function (silent) {
+				var wasLoggedUser = $scope.$root.user;
 
-            $scope.regOpenClick = function () {
-                //================analytics========================
-                //Нажатие на ссылку Регистрация/Вход
-                track.registrationOpen();
-                //================analytics========================
-                $scope.open();
-            }
+				$scope.$root.user = null;
 
-            $scope.open = function(){
-                utils.scrollFix()
-                $scope.isLoginPopupOpened = true;
-            };
+				if (!silent) {
+					AuthDataProvider.logout(onLogoutCompleteOrError, onLogoutCompleteOrError);
 
-            $scope.signInWith = function(method){
+					function onLogoutCompleteOrError() {
+						$scope.$emit(Events.AUTH_SIGN_OUT, wasLoggedUser);
+					}
+				}
+			};
 
-                var brokerWindow = window.open(AuthDataProvider.socialBrockerURL(method), "SocialBroker", "width=500,height=500,resizable=yes,scrollbars=no,status=no");
+			$scope.regOpenClick = function () {
+				//================analytics========================
+				//Нажатие на ссылку Регистрация/Вход
+				track.registrationOpen();
+				//================analytics========================
+				$scope.open();
+			}
 
-                brokerWindow.focus();
+			$scope.open = function () {
+				utils.scrollFix()
+				$scope.isLoginPopupOpened = true;
+			};
 
-                var socialBrokerListener = $('#social-broker-listener');
+			//setInterval(function(){
+			//    if(!$scope.$root.user){
+			//        //$scope.open();
+			//        $('.header__user-nav-reg').trigger('click');
+			//    }
+			//}, 500)
 
-                var interval = setInterval(function(){
-                    var cookieCloser = localStorage.getItem('closeSocialBroker');
+			/**
+			 * задача IN-4485
+			 * Костыль для спутника, если пользователь не залогинен, показываем ему форму логина всегда и везде
+			 */
+			if (partner != null && partner.name == 'sputnik') {
+				$scope.$root.$watch('user', function (data) {
+					if (!data) {
+						$scope.open();
+					}
+				});
+			}
+			/**
+			 * задача - IN-4485
+			 * Если у нас партнер спутник, скрываем форму регистрации
+			 */
+			if (partner != null && partner.name == 'sputnik') {
+				$scope.partnerSputnik = true;
+			}
+			else {
+				$scope.partnerSputnik = false;
+			}
 
-                    //console.log('cookieCloser', cookieCloser);
 
-                    if(cookieCloser) {
-                        localStorage.setItem('closeSocialBroker', 0);
+			$scope.signInWith = function (method) {
 
-                        login();
-                    }
-                }, 100);
+				var brokerWindow = window.open(AuthDataProvider.socialBrockerURL(method), "SocialBroker", "width=500,height=500,resizable=yes,scrollbars=no,status=no");
 
-                var login = function(){
-                    AuthDataProvider.recognize(function (data) {
-                        console.log('auth success:', method);
+				brokerWindow.focus();
 
-                        //analytics
-                        trackLogin(method);
+				var socialBrokerListener = $('#social-broker-listener');
 
-                        setUserInfo(data);
-                        $scope.close();
-                    }, function (err, data) {
-                        console.log('auth err:', err, data);
-                    });
+				var interval = setInterval(function () {
+					var cookieCloser = localStorage.getItem('closeSocialBroker');
 
-                    clearInterval(interval);
+					//console.log('cookieCloser', cookieCloser);
 
-                    socialBrokerListener.off('inna.Auth.SocialBroker.Result', login);
-                };
+					if (cookieCloser) {
+						localStorage.setItem('closeSocialBroker', 0);
 
-                function trackLogin(method) {
-                    switch (method) {
-                        case 'facebook': track.loginFbSuccess(); break;
-                        case 'google': track.loginGmailSuccess(); break;
-                        case 'vkontakte': track.loginVkSuccess(); break;
-                        case 'twitter': track.loginOkSuccess(); break;
-                        case 'odnoklassniki': track.loginTwSuccess(); break;
-                    }
-                }
+						login();
+					}
+				}, 100);
 
-                socialBrokerListener.on('inna.Auth.SocialBroker.Result', login);
+				var login = function () {
+					AuthDataProvider.recognize(function (data) {
+						console.log('auth success:', method);
 
-                return false;
-            };
+						//analytics
+						trackLogin(method);
 
-            $scope.showProfile = function ($event) {
-            	if ($scope.$root.user) {
-            	    //console.log('user.Type:', $scope.$root.user.getType());
-            	    switch ($scope.$root.user.getType()) {
-            	        case 2: {//B2B = 2, b2b.inna.ru
-            	            window.location = $scope.B2B_HOST;
-            	            $event.preventDefault();
-            	            return;
-            	        }
-            	        case 4: {//Partner = 4, partner.inna.ru
-            	            window.location = $scope.b2bPartnerHost;
-            	            $event.preventDefault();
-            	            return;
-            	        }
-            	    }
+						setUserInfo(data);
+						$scope.close();
+					}, function (err, data) {
+						console.log('auth err:', err, data);
+					});
 
-            	}
+					clearInterval(interval);
 
-                $scope.open();
-                $scope.display = $scope.DISPLAY_PROFILE;
-            };
+					socialBrokerListener.off('inna.Auth.SocialBroker.Result', login);
+				};
 
-            $scope.forgotten = function(){
-                $scope.display = $scope.DISPLAY_FORGOTTEN;
-            };
+				function trackLogin(method) {
+					switch (method) {
+						case 'facebook':
+							track.loginFbSuccess();
+							break;
+						case 'google':
+							track.loginGmailSuccess();
+							break;
+						case 'vkontakte':
+							track.loginVkSuccess();
+							break;
+						case 'twitter':
+							track.loginOkSuccess();
+							break;
+						case 'odnoklassniki':
+							track.loginTwSuccess();
+							break;
+					}
+				}
 
-            /*Properties*/
-            $scope.restoreToken = ($location.path() == app.URL_AUTH_RESTORE) && $location.search().token;
-            $scope.signUpToken = ($location.path() == app.URL_AUTH_SIGNUP) && $location.search().token;
+				socialBrokerListener.on('inna.Auth.SocialBroker.Result', login);
 
-            $scope.DISPLAY_SIGNIN = 1;
-            $scope.DISPLAY_FORGOTTEN = 2;
-            $scope.DISPLAY_SIGNUP = 3;
-            $scope.DISPLAY_PROFILE = 4;
+				return false;
+			};
 
-            $scope.isLoginPopupOpened = false;
+			$scope.showProfile = function ($event) {
+				if ($scope.$root.user) {
+					//console.log('user.Type:', $scope.$root.user.getType());
+					switch ($scope.$root.user.getType()) {
+						case 2:
+						{//B2B = 2, b2b.inna.ru
+							window.location = $scope.B2B_HOST;
+							$event.preventDefault();
+							return;
+						}
+						case 4:
+						{//Partner = 4, partner.inna.ru
+							window.location = $scope.b2bPartnerHost;
+							$event.preventDefault();
+							return;
+						}
+					}
 
-            $scope.display = $scope.DISPLAY_SIGNIN;
+				}
 
-            if($scope.restoreToken) {
-                $scope.display = $scope.DISPLAY_FORGOTTEN;
-                $scope.open();
-            } else if($scope.signUpToken) {
-                $scope.display = $scope.DISPLAY_SIGNUP;
-                $scope.open();
-            }
+				$scope.open();
+				$scope.display = $scope.DISPLAY_PROFILE;
+			};
 
-            $scope.recognize = function(needInitLastUserAfterLoginCheck){
-                AuthDataProvider.recognize(function (data) {
-                    setUserInfo(data, needInitLastUserAfterLoginCheck);
-                },
-                    function (err) {
-                        $scope.safeApply(function () {
-                            //выходим "по-тихому", без запросов на сервер и генерации события логаута
-                            $scope.logout(true);
+			$scope.forgotten = function () {
+				$scope.display = $scope.DISPLAY_FORGOTTEN;
+			};
 
-                            //проверяем, нужно ли перезагрузить страницу
-                            if ($scope.reloadChecker) {
-                                $scope.reloadChecker.checkReloadPage();
-                            }
-                        });
-                    });
-            }
+			/*Properties*/
+			$scope.restoreToken = ($location.path() == app.URL_AUTH_RESTORE) && $location.search().token;
+			$scope.signUpToken = ($location.path() == app.URL_AUTH_SIGNUP) && $location.search().token;
 
-            $scope.B2B_HOST = window.DEV && window.DEV_B2B_HOST || app_main.b2bHost;
-            $scope.b2bPartnerHost = app_main.b2bPartnerHost;
+			$scope.DISPLAY_SIGNIN = 1;
+			$scope.DISPLAY_FORGOTTEN = 2;
+			$scope.DISPLAY_SIGNUP = 3;
+			$scope.DISPLAY_PROFILE = 4;
 
-            /*EventListeners*/
-            $scope.$on(Events.AUTH_SIGN_IN, function (event, data) {
-                $scope.safeApply(function(){
-                    setUserInfo(data);
-                    $scope.close();
+			$scope.isLoginPopupOpened = false;
+			
+			$scope.display = $scope.DISPLAY_SIGNIN;
 
-                    $scope.reloadChecker.saveLastUser();
+			if ($scope.restoreToken) {
+				$scope.display = $scope.DISPLAY_FORGOTTEN;
+				$scope.open();
+			} else if ($scope.signUpToken) {
+				$scope.display = $scope.DISPLAY_SIGNUP;
+				$scope.open();
+			}
 
-                    if ($scope.$root.user) {
-                        //console.log('user.Type:', $scope.$root.user.getType());
-                        switch ($scope.$root.user.getType()) {
-                            case 2: {//B2B = 2, b2b.inna.ru
-                                window.location = $scope.B2B_HOST;
-                                break;
-                            }
-                            case 4: {//Partner = 4, partner.inna.ru
-                                window.location = $scope.b2bPartnerHost;
-                                break;
-                            }
-                        }
-                        
-                    }
-                });
-            });
+			$scope.recognize = function (needInitLastUserAfterLoginCheck) {
+				AuthDataProvider.recognize(function (data) {
+						setUserInfo(data, needInitLastUserAfterLoginCheck);
+					},
+					function (err) {
+						$scope.safeApply(function () {
+							//выходим "по-тихому", без запросов на сервер и генерации события логаута
+							$scope.logout(true);
 
-            $scope.$on(Events.AUTH_SIGN_OUT, function(event, userRaw){
-                var user = new modelAuth(userRaw.raw);
+							//проверяем, нужно ли перезагрузить страницу
+							if ($scope.reloadChecker) {
+								$scope.reloadChecker.checkReloadPage();
+							}
+						});
+					});
+			}
 
-                if(user.isAgency() && !user.raw.AgencyActive) {
-                    $scope.baloon.showErr('Агентство неактивно', 'Вход не возможен', function(){
-                        window.location = '/';
-                    });
-                } else if(user.isAgency()) {
+			$scope.B2B_HOST = window.DEV && window.DEV_B2B_HOST || app_main.b2bHost;
+			$scope.b2bPartnerHost = app_main.b2bPartnerHost;
 
-                    window.location = '/';
-                }
-            });
+			/*EventListeners*/
+			$scope.$on(Events.AUTH_SIGN_IN, function (event, data) {
+				$scope.safeApply(function () {
+					setUserInfo(data);
+					$scope.close();
 
-            /*Initial*/
-            $scope.recognize(true);
+					$scope.reloadChecker.saveLastUser();
 
-            //поддержка залогиненности из нескольких вкладок
-            function reloadChecker() {
-                var self = this;
+					if ($scope.$root.user) {
+						//console.log('user.Type:', $scope.$root.user.getType());
+						switch ($scope.$root.user.getType()) {
+							case 2:
+							{//B2B = 2, b2b.inna.ru
+								window.location = $scope.B2B_HOST;
+								break;
+							}
+							case 4:
+							{//Partner = 4, partner.inna.ru
+								window.location = $scope.b2bPartnerHost;
+								break;
+							}
+						}
 
-                self.lastLoginUser = null;
+					}
+				});
+			});
 
-                //events
-                self.init = function () {
-                    $(window).on('focus', function () {
-                        //тут проверяем авторизацию
-                        //и если она изменилась - то перезагружаем страницу
-                        $scope.recognize();
-                    });
-                };
+			$scope.$on(Events.AUTH_SIGN_OUT, function (event, userRaw) {
+				var user = new modelAuth(userRaw.raw);
 
-                self.getCurrentUser = function () {
-                    return ($scope.$root.user && $scope.$root.user.raw) ? $scope.$root.user.raw.Email : null;
-                }
+				if (user.isAgency() && !user.raw.AgencyActive) {
+					$scope.baloon.showErr('Агентство неактивно', 'Вход не возможен', function () {
+						window.location = '/';
+					});
+				} else if (user.isAgency()) {
 
-                self.saveLastUser = function (curUser) {
-                    if (curUser == null) {
-                        curUser = self.getCurrentUser();
-                    }
-                    self.lastLoginUser = curUser;
-                };
+					window.location = '/';
+				}
+			});
 
-                self.destroy = function () {
-                    $(window).off('focus');
-                }
+			/*Initial*/
+			$scope.recognize(true);
 
-                self.checkReloadPage = function () {
-                    var curUser = self.getCurrentUser();
-                    //console.log('checkReloadPage, curUser: ' + curUser + ' lastLoginUser: ' + self.lastLoginUser);
-                    //состояние залогиненности изменилось - тригерим событие
-                    if (self.lastLoginUser != curUser) {
-                        //решрешим страницу
-                        $route.reload();
-                    }
-                    self.saveLastUser(curUser);
-                };
+			//поддержка залогиненности из нескольких вкладок
+			function reloadChecker() {
+				var self = this;
 
-                self.init();
-            }
+				self.lastLoginUser = null;
 
-            $scope.reloadChecker = new reloadChecker();
-            
+				//events
+				self.init = function () {
+					$(window).on('focus', function () {
+						//тут проверяем авторизацию
+						//и если она изменилась - то перезагружаем страницу
+						$scope.recognize();
+					});
+				};
 
-            $scope.$on('$destroy', function () {
-                reloadChecker.destroy();
-            });
-        }
-    ]);
+				self.getCurrentUser = function () {
+					return ($scope.$root.user && $scope.$root.user.raw) ? $scope.$root.user.raw.Email : null;
+				}
+
+				self.saveLastUser = function (curUser) {
+					if (curUser == null) {
+						curUser = self.getCurrentUser();
+					}
+					self.lastLoginUser = curUser;
+				};
+
+				self.destroy = function () {
+					$(window).off('focus');
+				}
+
+				self.checkReloadPage = function () {
+					var curUser = self.getCurrentUser();
+					//console.log('checkReloadPage, curUser: ' + curUser + ' lastLoginUser: ' + self.lastLoginUser);
+					//состояние залогиненности изменилось - тригерим событие
+					if (self.lastLoginUser != curUser) {
+						//решрешим страницу
+						$route.reload();
+					}
+					self.saveLastUser(curUser);
+				};
+
+				self.init();
+			}
+
+			$scope.reloadChecker = new reloadChecker();
+
+
+			$scope.$on('$destroy', function () {
+				reloadChecker.destroy();
+			});
+		}
+	]);
