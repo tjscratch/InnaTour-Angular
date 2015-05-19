2d56db692d2f750a78128de0c55aa48d7e09b3af
Alexander Sveshnikov <diamondliquid@gmail.com>
2015-05-19 16:59:41 +0300

    font fix

--- node-app/partners/data.js
+++ node-app/partners/data.js
@@ -19,7 +19,7 @@ module.exports = {
             }
         }
 
-        if (partner && partnerName == 'sputnik'){
+        if (partner && data.name == 'sputnik'){
             partner.isSputnik = true;
         }
 
