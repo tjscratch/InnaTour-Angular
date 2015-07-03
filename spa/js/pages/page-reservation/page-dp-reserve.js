0b730ceefb02bbd91447e630b8fa9bdb41946ec3
maxstbn <maxstbn@gmail.com>
2015-07-03 17:26:22 +0300

    правильные редиректы при замене комнаты и билета

--- spa/js/pages/page-reservation/page-dp-reserve.js
+++ spa/js/pages/page-reservation/page-dp-reserve.js
@@ -114,6 +114,7 @@
                 $location.url(url);
             }
 
+            //http://lh.inna.ru/#/packages/search/6733-6623-17.08.2015-23.08.2015-0-2-?hotel=397940&ticket=10000145429&display=tickets
             $scope.goBackUrl = function () {
                 var url = '/#' + Urls.URL_DYNAMIC_PACKAGES_SEARCH +
                     [
@@ -204,19 +205,43 @@
             }
 
 
+            //http://lh.inna.ru/#/packages/search/6733-6623-17.08.2015-23.08.2015-0-2-?hotel=397940&ticket=10000145429&display=tickets
+            function goToAvia() {
+                var url = Urls.URL_DYNAMIC_PACKAGES_SEARCH +
+                    [
+                        $routeParams.DepartureId,
+                        $routeParams.ArrivalId,
+                        $routeParams.StartVoyageDate,
+                        $routeParams.EndVoyageDate,
+                        $routeParams.TicketClass,
+                        $routeParams.Adult,
+                        $routeParams.Children > 0 ? $routeParams.Children : '',
+                        "?display=tickets&hotel=" + $scope.hotel.HotelId + "&ticket=" + $scope.item.VariantId1
+                    ].join('-');
+                var url = url.replace('--', '-');
+                $location.url(url);
+            };
+
+
+            function goToHotelDetails() {
+                var detailsUrl = $scope.getHotelInfoLink($scope.item.VariantId1, $scope.item.VariantId2, $scope.hotel.HotelId, $scope.hotel.ProviderId);
+                var url = detailsUrl.replace('/#', '');
+                $location.url(url);
+            }
+
+
             function noAvailability(data) {
 
-                
+
                 function showBaloon(text1, text2) {
                     $scope.safeApply(function () {
                         $scope.baloon.showNotFound(text1, text2,
                             function () {
                                 $timeout.cancel($scope.tmId);
-                                goToSearch();
+                                //goToSearch();
                             });
                     });
                 }
-
 
                 if (data != null) {
                     if (data.IsTicketAvailable == false) {
@@ -232,10 +257,15 @@
                     storageService.clearAviaSearchResults();
                     $scope.baloon.hide();
                     //билеты не доступны - отправляем на поиск
-                    goToSearch();
+                    //goToSearch();
+                    if (data.IsTicketAvailable == false) {
+                        goToAvia();
+                    }
+                    if (data.Rooms != null && data.Rooms.length && data.Rooms[0].IsAvailable == false) {
+                        goToHotelDetails();
+                    }
                 }, 3000);
             }
-
 
             /**
              * проверяем, что остались билеты для покупки
@@ -255,6 +285,7 @@
                             //правила отмены отеля
                             $scope.hotelRules.fillData($scope.hotel);
 
+                            noAvailability(data);
                             //загружаем все
                             $scope.initPayModel();
                         }
