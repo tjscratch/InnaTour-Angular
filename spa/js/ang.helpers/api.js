1306a450229802915e4ffbf033cf7390d568e0ac
Alexander Sveshnikov <diamondliquid@gmail.com>
2015-09-04 16:49:38 +0300

    debugApiHost

--- spa/js/ang.helpers/api.js
+++ spa/js/ang.helpers/api.js
@@ -1,10 +1,17 @@
 angular.module('innaApp.API', [])
     .factory('innaApp.API.const', function () {
-        function url(s) {
+        function url(s, apiHost) {
             var host = '';
             host = host || '';
 
-            return host + '/api/v1' + s;
+            var result = host + '/api/v1' + s;
+
+            var debugApiHost = localStorage.getItem('debug_api_host');
+            if (debugApiHost && debugApiHost.length > 0 && debugApiHost.indexOf('http') > -1) {
+                result = debugApiHost + result;
+            }
+
+            return result;
         }
 
         return {
