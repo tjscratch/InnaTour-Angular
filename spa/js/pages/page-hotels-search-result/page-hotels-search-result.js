448f76704958b129f0db05e86c2a1ac234072a3d
Maksim Stolbukhin <maxstbn@gmail.com>
2016-03-27 15:00:40 +0300

    прелоадер

--- spa/js/pages/page-hotels-search-result/page-hotels-search-result.js
+++ spa/js/pages/page-hotels-search-result/page-hotels-search-result.js
@@ -1,13 +1,47 @@
-innaAppControllers.controller('PageHotelsSearchResultCtrl', function ($scope, $routeParams, HotelService, ListPanel) {
+innaAppControllers.controller('PageHotelsSearchResultCtrl', function ($scope, $routeParams, $location,
+                                                                      refactoringAppUrls, Balloon, HotelService) {
+
+    /**
+     * при переходе на данную страницу
+     * показываем прелоадер на время
+     * получения ответа с результатами поиска
+     */
+    $scope.baloonHotelLoad = new Balloon();
+    $scope.baloonHotelLoad.updateView({
+        template: 'search.html',
+        callbackClose: function () {
+            $scope.redirectHotels();
+        },
+        callback: function () {
+            $scope.redirectHotels();
+        }
+    });
+
+
+    /**
+     * клик на балуне, по кнопке закрыть или "прервать поиск"
+     * редиректим на /hotels/
+     */
+    $scope.redirectHotels = function () {
+        $scope.baloonHotelLoad.teardown();
+        $location.path(refactoringAppUrls.URL_HOTELS);
+    };
+
 
     if ($routeParams) {
+        $scope.baloonHotelLoad.show();
         HotelService.getHotelsList($routeParams)
             .success(function (data) {
-                console.log('HotelService.getHotelsList')
-                console.log(data)
                 $scope.hotels = data.Hotels;
+                $scope.baloonHotelLoad.teardown();
             })
     }
 
 
+    $scope.$on('$destroy', function () {
+        if ($scope.baloonHotelLoad) {
+            $scope.baloonHotelLoad.teardown();
+            $scope.baloonHotelLoad = null;
+        }
+    });
 });
