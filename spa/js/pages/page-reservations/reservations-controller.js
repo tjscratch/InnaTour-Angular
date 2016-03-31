157c53b4b25fb34e5b82943fcd983748eea98908
Maxim Stolbukhin <maxstbn@gmail.com>
2016-03-31 19:13:06 +0300

    валидация

--- spa/js/pages/page-reservations/reservations-controller.js
+++ spa/js/pages/page-reservations/reservations-controller.js
@@ -1,19 +1,21 @@
+/**
+ * для валидации используется библиотека
+ * https://github.com/huei90/angular-validation
+ */
 innaAppControllers.controller('ReservationsController', function ($scope, $routeParams, $location, $injector, ReservationService) {
 
     var $validationProvider = $injector.get('$validation');
     // если в url есть параметр ?test=1
     // заполняем данные пассажира фейковыми данными
-    if($location.$$search && $location.$$search.test == 1){
+    if ($location.$$search && $location.$$search.test == 1) {
         $scope.ReservationModel = ReservationService.getReservationModel($routeParams.Adult, 1);
-    }else{
+    } else {
         $scope.ReservationModel = ReservationService.getReservationModel($routeParams.Adult);
     }
 
     $scope.ReservationForm = {
         checkValid: $validationProvider.checkValid
     };
-
-
 
 
 });
