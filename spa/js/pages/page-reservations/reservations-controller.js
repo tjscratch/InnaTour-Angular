2e184480d1cbe734594c3b904b16a1e5ae717d68
Maxim Stolbukhin <maxstbn@gmail.com>
2016-04-05 12:38:00 +0300

    angular ui-select validation

--- spa/js/pages/page-reservations/reservations-controller.js
+++ spa/js/pages/page-reservations/reservations-controller.js
@@ -52,44 +52,8 @@ innaAppControllers.controller('ReservationsController', function ($scope, $route
         .success(function (data) {
             $scope.countries = data;
         })
+        .error(function (data) {
+            console.log('ReservationService.countries: ' + data);
+        })
 
-    // $scope.$watchCollection('uiSelectCountry', function (data) {
-    //     if(data.selected){
-    //         console.log(data.selected)
-    //     }
-    // })
-
-
-});
-
-
-innaAppFilters.filter('propsFilter', function () {
-    return function (items, props) {
-        var out = [];
-
-        if (angular.isArray(items)) {
-            items.forEach(function (item) {
-                var itemMatches = false;
-
-                var keys = Object.keys(props);
-                for (var i = 0; i < keys.length; i++) {
-                    var prop = keys[i];
-                    var text = props[prop].toLowerCase();
-                    if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-                        itemMatches = true;
-                        break;
-                    }
-                }
-
-                if (itemMatches) {
-                    out.push(item);
-                }
-            });
-        } else {
-            // Let the output be the input untouched
-            out = items;
-        }
-
-        return out;
-    }
 });
