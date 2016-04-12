e9ce943d9f13a3ec328e994e0ea8bef9b6caec12
Maxim Stolbukhin <maxstbn@gmail.com>
2016-04-12 16:07:26 +0300

    фиксы видеочата

--- spa/js/services/refactoring.service.manager.js
+++ spa/js/services/refactoring.service.manager.js
@@ -9,9 +9,15 @@ innaAppServices.service('ManagerService', function ($http, appApi, $q) {
                 method: 'GET'
             }).then(function (res) {
                 if (res.status == 200) {
-                    if(res.data['Data']['meetings']['meeting']['running']){
-                        deferred.resolve(true);
-                    }else {
+                    if (res.data['Data']) {
+                        if (res.data['Data']['meetings']) {
+                            if (res.data['Data']['meetings']['meeting']) {
+                                if (res.data['Data']['meetings']['meeting']['running']) {
+                                    deferred.resolve(true);
+                                }
+                            }
+                        }
+                    } else {
                         deferred.resolve(false);
                     }
                 } else {
