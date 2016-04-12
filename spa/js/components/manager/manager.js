ef3484a7542f71309425a60389b1988cbc940e06
Maxim Stolbukhin <maxstbn@gmail.com>
2016-04-12 13:39:06 +0300

    видеочат как виджет

--- spa/js/components/manager/manager.js
+++ spa/js/components/manager/manager.js
@@ -2,11 +2,6 @@ innaAppDirectives.directive('manager', function ($templateCache, $interval, $tim
     return {
         replace: true,
         template: $templateCache.get("components/manager/templ/index.html"),
-        //scope: {
-        //    orderId: '=',
-        //    price: '=',
-        //    uid: '='
-        //},
         link: function ($scope, element, attrs) {
             var url = "//manager.inna.ru/";
 
@@ -20,7 +15,7 @@ innaAppDirectives.directive('manager', function ($templateCache, $interval, $tim
                         if (data.Data.meetings.meeting) {
                             if (data.Data.meetings.meeting.running) {
                                 $scope.showChat = true;
-                                $timeout(function (){
+                                $timeout(function () {
                                     $scope.showChatManager = true;
                                 }, 10000)
                             } else {
@@ -75,6 +70,87 @@ innaAppDirectives.directive('manager', function ($templateCache, $interval, $tim
                 $(".b-manager").append(managerContainer);
             };
 
+        }
+    }
+});
+
+
+innaAppDirectives.directive('managerWidget', function ($templateCache, $interval, $timeout, ManagerService) {
+    return {
+        replace: true,
+        template: $templateCache.get("components/manager/templ/widget.html"),
+        link: function ($scope, element, attrs) {
+            var url = "//manager.inna.ru/";
+
+            $scope.url = url;
+            $scope.openWidget = false;
+            $scope.openFullWidth = false;
+
+            // $scope.showChat = false;
+            // $scope.showChatManager = false;
+
+
+            $scope.toggleOpenWidget = function () {
+                $scope.openWidget = !$scope.openWidget;
+
+                if(!$scope.openWidget){
+                    $scope.openFullWidth = false;
+                }
+            };
+
+            //fullWidth
+            $scope.toggleOpenFullWidth = function () {
+                $scope.openFullWidth = !$scope.openFullWidth;
+            };
+
+
+            function setManager () {
+                ManagerService.getManagerStatus()
+                    .success(function (data) {
+                        if (data.Data.meetings.meeting) {
+                            if (data.Data.meetings.meeting.running) {
+                                $scope.showChat = true;
+                                $timeout(function () {
+                                    $scope.showChatManager = true;
+                                }, 10000)
+                            } else {
+                                $scope.showChat = false;
+                                $scope.showChatManager = false;
+                            }
+                        } else {
+                            $scope.showChat = false;
+                            $scope.showChatManager = false;
+                        }
+                    })
+            }
+
+            //setManager();
+
+
+            var stop;
+            $scope.fight = function () {
+                // Don't start a new fight if we are already fighting
+                if (angular.isDefined(stop)) return;
+
+                stop = $interval(function () {
+                    setManager();
+                }, 4000);
+            };
+
+            $scope.stopFight = function () {
+                if (angular.isDefined(stop)) {
+                    $interval.cancel(stop);
+                    stop = undefined;
+                }
+            };
+
+            $scope.fight();
+
+
+            $scope.$on('$destroy', function () {
+                // Make sure that the interval is destroyed too
+                $scope.stopFight();
+            });
         }
     }
 });
