f662d39cbc96b4d7f3a4dad1176b026ef2b34717
Maxim Stolbukhin <maxstbn@gmail.com>
2016-04-20 17:39:16 +0300

    fix

--- spa/js/services/refactoring.service.manager.js
+++ spa/js/services/refactoring.service.manager.js
@@ -1,21 +1,19 @@
 innaAppServices.service('ManagerService', function ($http, appApi, $q) {
     return {
         getManagerStatus: function () {
-
+            
             var deferred = $q.defer();
-
+            
             $http({
                 url: appApi.GET_MANAGER_STATUS,
                 method: 'GET'
             }).then(function (res) {
                 if (res.status == 200) {
                     if (res.data['Data']) {
-                        if (res.data['Data']['meetings']) {
-                            if (res.data['Data']['meetings']['meeting']) {
-                                if (res.data['Data']['meetings']['meeting']['running']) {
-                                    deferred.resolve(true);
-                                }
-                            }
+                        if (res.data['Data']['moderatorCount'] > 0) {
+                            deferred.resolve(true);
+                        }else{
+                            deferred.resolve(false);
                         }
                     } else {
                         deferred.resolve(false);
@@ -26,7 +24,7 @@ innaAppServices.service('ManagerService', function ($http, appApi, $q) {
             }, function (res) {
                 deferred.resolve(false);
             });
-
+            
             return deferred.promise;
         }
     }
