d9c95d27ff5a1944a4a183fb1020e8c677db2052
maxstbn <maxstbn@gmail.com>
2016-05-17 16:43:42 +0300

    рефакторинг офферов

--- spa/js/components/offers/offers.js
+++ spa/js/components/offers/offers.js
@@ -7,24 +7,6 @@ innaAppDirectives.directive('offers', function ($templateCache) {
         },
         controller: function ($scope, serviceCache, Offer) {
 
-            // /BestOffer/GetOffers?Location=6733&Period=0&Category=0&Month=0
-
-            $scope.filter = {
-                Category: null,
-                Location: null,
-                Month: null,
-                Period: null
-            };
-
-            Offer.getOffers().then(
-                function (res) {
-                    setDefaultValue(res);
-                }, function (res) {
-                    console.log('/BestOffer/GetOffers ERROR');
-                    console.log(res);
-                });
-
-
             function setDefaultValue (res) {
 
                 $scope.Categories = res.data.Categories;
@@ -66,7 +48,22 @@ innaAppDirectives.directive('offers', function ($templateCache) {
                 });
                 $scope.Sort = SortObj.Value;
                 $scope.offersSort($scope.Sort);
-            }
+            };
+
+            $scope.filter = {
+                Category: null,
+                Location: null,
+                Month: null,
+                Period: null
+            };
+
+            Offer.getOffers().then(
+                function (res) {
+                    setDefaultValue(res);
+                }, function (res) {
+                    console.log('/BestOffer/GetOffers ERROR');
+                    console.log(res);
+                });
 
 
             $scope.filterChange = function (filter) {
@@ -80,57 +77,9 @@ innaAppDirectives.directive('offers', function ($templateCache) {
 
             };
 
-//
-//
-//
-//
-//
-//
-// генерация офферов
-//
-//
-//
-//
-//
-//
-            function randomInteger (min, max) {
-                var rand = min + Math.random() * (max - min)
-                rand = Math.round(rand);
-                return rand;
-            }
-
-            var offers = [];
-            for (var i = 0; i < 12; i++) {
-                var offer = {
-                    img: "http://s.test.inna.ru/Files/Photos/140221190450/140221191248/p_960x428.jpg",
-                    titleSub: "Куба",
-                    titleMain: "Гавана",
-                    titleInfo: "7 ночей, на двоих",
-                    priceTxt: "от",
-                    priceValue: randomInteger(20000, 300000),
-                    popular: randomInteger(1, 100)
-                };
-                offers.push(offer);
-            }
-
+            var offers = Offer.mock(12);
             $scope.offersSort = function (sortableType) {
-                var sortOffers;
-                if (sortableType == 0) {
-                    sortOffers = _.sortBy(offers, function (o) {
-                        return o.popular;
-                    });
-                }
-                if (sortableType == 1) {
-                    sortOffers = _.sortBy(offers, function (o) {
-                        return o.priceValue;
-                    });
-                }
-                if (sortableType == 2) {
-                    sortOffers = _.sortBy(offers, function (o) {
-                        return -o.priceValue;
-                    });
-                }
-                $scope.offers = _.partitionArray(sortOffers, 3);
+                $scope.offers = Offer.sortable(sortableType, offers);
             };
 
         }
