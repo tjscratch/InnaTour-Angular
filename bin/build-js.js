1ad80618e63b7d9d49484ea163ebe22c5b84a264
maximHome <maxstbn@gmail.com>
2016-05-27 20:28:55 +0300

    сборка js

---
+++ bin/build-js.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglifyjs');
+var gulpif = require('gulp-if');
+var livereload = require('gulp-livereload');
+var ngAnnotate = require('gulp-ng-annotate');
+var templateCache = require('gulp-angular-templatecache');
+var cleanhtml = require('gulp-cleanhtml');
+var config = require('./config');
+
+
+var _ENV_ = process.env.NODE_ENV || 'DEV';
+
+
+// сборка js библиотек в один файл
+gulp.task('build-libs', function () {
+    return gulp.src(config.js.bowerSrcs)
+        .pipe(gulpif(_ENV_ != 'production', sourcemaps.init()))
+        .pipe(concat('vendor.js'))
+        .pipe(gulpif(_ENV_ == 'production',
+            uglify({
+                mangle: true,
+                compress: true,
+                output: {
+                    beautify: false
+                }
+            })
+        ))
+        .pipe(gulpif(_ENV_ != 'production', sourcemaps.write()))
+        .pipe(gulp.dest(config.js.distSrc));
+});
+
+
+// сборка темплейтов
+gulp.task('build-angular-templates', function () {
+    return gulp.src(config.templates.src)
+        .pipe(gulpif(_ENV_ != 'production', sourcemaps.init()))
+        .pipe(gulpif(_ENV_ == 'production', cleanhtml()))
+        .pipe(templateCache({
+            module: config.templates.angularModuleName
+        }))
+        .pipe(gulpif(_ENV_ == 'production',
+            uglify({
+                mangle: true,
+                compress: true,
+                output: {
+                    beautify: false
+                }
+            })
+        ))
+        .pipe(gulpif(_ENV_ != 'production', sourcemaps.write()))
+        .pipe(gulp.dest(config.templates.distSrc));
+});
+
+gulp.task('build-angular-templates-watch', function () {
+    gulp.watch(config.templates.src, ['build-angular-templates'])
+});
--- bin/config.js
+++ bin/config.js
@@ -2,16 +2,6 @@ var config = {
     "dist": {
         "src": "./dist"
     },
-    "templates": {
-        "src": [
-            "./spa/templates/**/*.html",
-            "./spa/js/**/*.html",
-            "./spa/js/widgets/search/templ/*.html"
-        ],
-        "angularModuleName": "innaApp.templates",
-        "distSrc": "./build/js",
-        "distSrcMd5": "./build/js/*.js"
-    },
     "nodeApp": {
         "distLayouts": "./dist/node-app/templates/layouts/*.hbs"
     },
@@ -23,7 +13,52 @@ var config = {
         "src": "",
         "distSrc": "./dist/spa/css",
         "distSrcMd5": "./dist/spa/css/*.css"
-    }
+    },
+    "templates": {
+        "src": [
+            "./spa/templates/**/*.html",
+            "./spa/js/**/*.html",
+            "./spa/js/widgets/search/templ/*.html"
+        ],
+        "angularModuleName": "innaApp.templates",
+        "distSrc": "./build/js",
+        "distSrcMd5": "./build/js/*.js"
+    },
+    "js": {
+        "src": "",
+        "distSrc": "./build/js",
+        "distSrcMd5": "./build/js/*.js",
+        "bowerSrcs": [
+            "./bower_components/underscore/underscore-min.js",
+            "./bower_components/raven-js/dist/raven.min.js",
+            "./bower_components/jquery/dist/jquery.min.js",
+            "./bower_components/ractive/ractive.min.js",
+            "./bower_components/ractive-events-hover/ractive-events-hover.js",
+            "./bower_components/angular/angular.min.js",
+            "./bower_components/angular-cookies/angular-cookies.min.js",
+            "./bower_components/angular-locale-ru/angular-locale_ru.js",
+            "./bower_components/angular-sanitize/angular-sanitize.min.js",
+            "./bower_components/angular-route/angular-route.min.js",
+            "./bower_components/angular-touch/angular-touch.min.js",
+            "./bower_components/angular-hotkeys/build/hotkeys.min.js",
+            "./bower_components/moment/min/moment.min.js",
+            "./bower_components/moment/locale/ru.js",
+            "./bower_components/angular-ui-scroll/dist/ui-scroll.min.js",
+            "./bower_components/angular-validation/dist/angular-validation.min.js",
+            "./bower_components/angular-validation/dist/angular-validation-rule.min.js",
+            "./bower_components/angular-ui-select/dist/select.min.js",
+            "./spa/lib/moment-ru.js",
+            "./spa/lib/polyfill/**/*.js",
+            "./spa/lib/jquery.maskedinput.js",
+            "./spa/lib/jquery.ui.datepicker-ru.js",
+            "./spa/lib/jquery.scrollTo.min.js",
+            "./spa/lib/datepicker/datepicker.js",
+            "./spa/lib/jquery-ui/jquery-ui.1.11.2.min.js",
+            "./spa/lib/ui-bootstrap-typeahead-custom/typeahead.js",
+            "./spa/lib/bootstrap-datepicker/bootstrap-datepicker.js",
+            "./spa/lib/bootstrap-datepicker/bootstrap-datepicker.ru.min.js"
+        ]
+    },
 }
 
 module.exports = config;
