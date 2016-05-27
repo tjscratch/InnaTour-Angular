16f351ea91c8d8f1a5ed73f0ee6b344d9714a9ba
maximHome <maxstbn@gmail.com>
2016-05-27 14:56:10 +0300

    генерация спрайтов

--- bin/build-sprites.js
+++ bin/build-sprites.js
@@ -1,6 +1,4 @@
-var conf = require('./config');
 var del = require('del');
-
 var gulp = require('gulp');
 var buffer = require('vinyl-buffer');
 var csso = require('gulp-csso');
@@ -10,20 +8,20 @@ var handlebars = require('handlebars');
 var spritesmith = require('gulp.spritesmith');
 
 
-var tmpl = require('./sprite.hbs');
+var tmpl = require('./sprite.handlebars');
 var spritesPath = './spa/img/sprites/**/*.png';
 var distSpritesCss = './spa/styl';
-var distSpritesImg = './dist/spa/img/sprites/build';
+var distSpritesImg = './spa/img/sprites/build';
 
 
 gulp.task('build-sprite-remove-build', function (cb) {
-    return del(conf.sprites + '/build', {
+    return del(distSpritesImg, {
         force: true
     }, cb);
 });
 
 gulp.task('build-sprite-remove-style', function (cb) {
-    return del(conf.styl + '/sprite.styl', {
+    return del(distSpritesCss + '/sprite.styl', {
         force: true
     }, cb);
 });
@@ -31,7 +29,7 @@ gulp.task('build-sprite-remove-style', function (cb) {
 
 gulp.task('build-sprite', ['build-sprite-remove-build', 'build-sprite-remove-style'], function () {
     
-    var spriteData = gulp.src(spritesPath + '/**/*.png').pipe(spritesmith({
+    var spriteData = gulp.src(spritesPath).pipe(spritesmith({
         imgName: 'sprite.png',
         cssName: 'sprite.styl',
         cssTemplate: tmpl
@@ -45,10 +43,8 @@ gulp.task('build-sprite', ['build-sprite-remove-build', 'build-sprite-remove-sty
     
     // Pipe CSS stream through CSS optimizer and onto disk
     var cssStream = spriteData.css
-    //.pipe(csso())
         .pipe(gulp.dest(distSpritesCss));
     
     // Return a merged stream to handle both `end` events
     return merge(imgStream, cssStream);
-    //return spriteData.pipe(gulp.dest(conf.sprites));
 });
