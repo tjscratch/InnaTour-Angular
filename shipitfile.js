fef900c5711e4f9c9a748acdd581eded6eacbbf4
maximHome <maxstbn@gmail.com>
2016-05-30 22:26:43 +0300

    новый деплой

--- shipitfile.js
+++ shipitfile.js
@@ -3,7 +3,7 @@ module.exports = function (shipit) {
 
 
     shipit.initConfig({
-        default: {
+        staging: {
             workspace: 'shipit_build',
             deployTo: '/home/deploy/www/inna-frontend',
             repositoryUrl: 'ssh://git@gitlab.inna.ru:223/frontend-dev/inna-angular.git',
@@ -11,12 +11,9 @@ module.exports = function (shipit) {
             ignores: ['.git', 'node_modules'],
             keepReleases: 20,
             deleteOnRollback: true,
-            //key: '~/.ssh/id_rsa.pub',
             shallowClone: false,
-            stopService: 'sudo service inna-frontend stop; ',
-            startService: 'sudo service inna-frontend start'
-        },
-        staging: {
+            restartService: 'sudo restart inna-frontend',
+            build: ' && NODE_ENV=production gulp build',
             servers: 'root@5.200.60.73:2223'
         },
         test: {
@@ -28,8 +25,21 @@ module.exports = function (shipit) {
             keepReleases: 5,
             deleteOnRollback: true,
             shallowClone: false,
-            stopService: 'sudo service inna-frontend-test stop; ',
-            startService: 'sudo service inna-frontend-test start',
+            restartService: 'sudo restart inna-frontend-test',
+            build: ' && NODE_ENV=test gulp build',
+            servers: 'deploy@5.200.60.73:2210',
+        },
+        beta: {
+            workspace: 'shipit_build_test',
+            deployTo: '/home/deploy/www/inna-frontend-test',
+            repositoryUrl: 'ssh://git@gitlab.inna.ru:223/frontend-dev/inna-angular.git',
+            branch: 'deploy',
+            ignores: ['.git', 'node_modules'],
+            keepReleases: 5,
+            deleteOnRollback: true,
+            shallowClone: false,
+            restartService: 'sudo restart inna-frontend-beta',
+            build: ' && NODE_ENV=beta gulp build',
             servers: 'deploy@5.200.60.73:2210',
         }
     });
@@ -52,7 +62,6 @@ module.exports = function (shipit) {
         return shipit.start(
             'after.deploy::copy.package.json',
             'after.deploy::run.npm.install',
-            //'after.deploy::run.build',
             'after.deploy::restart.service',
             'print.rollback'
         );
@@ -67,13 +76,12 @@ module.exports = function (shipit) {
 
     //собирает проект локально, перед копированием на сервер
     shipit.blTask('pre.deploy::build', function () {
-        return shipit.local('cd ' + shipit.config.workspace + '&& NODE_ENV=production gulp build');
+        return shipit.local('cd ' + shipit.config.workspace + shipit.config.build);
     });
 
 
     //копируем package.json в корневую папку,
     //чтобы каждый раз не устанавливать все пакеты заново
-    //для ускорения билда короче
     shipit.blTask('after.deploy::copy.package.json', function () {
         return shipit.remote('cd ' + shipit.currentPath + ' && cp package.json ' + shipit.config.deployTo);
     });
@@ -83,28 +91,7 @@ module.exports = function (shipit) {
         return shipit.remote('cd ' + shipit.config.deployTo + ' && npm install');
     });
 
-    //запускаем build --release в текущем билде
-    shipit.blTask('after.deploy::run.build', function () {
-        return shipit.remote('cd ' + shipit.currentPath + ' && NODE_ENV=production gulp build');
-    });
-
-    //перезапускаем приложение
-    //forever list | grep -q build.server.js - возвращает 0 - если не нашлось строки 'build.server.js', 1 - если нашлось
-    //и соответственно запускается команда
-    //shipit.blTask('after.deploy::restart.forever', function () {
-    //    var cmd = '';
-    //    cmd += ' forever list | grep -q build.server.js && forever stop '+ shipit.currentPath + '/build/server.js;';
-    //    cmd += ' forever start '+ shipit.currentPath + '/build/server.js;';
-    //    return shipit.remote(cmd);
-    //});
-
     shipit.blTask('after.deploy::restart.service', function () {
-        var cmd = '';
-        cmd += shipit.config.stopService;
-        cmd += shipit.config.startService;
-        return shipit.remote(cmd);
+        return shipit.remote(shipit.config.restartService);
     });
 };
-
-
-//ssh -p 2223 root@5.200.60.73 "cd /home/deploy/www/inna-react/current; forever list | grep -q build.server.js && forever restart build/server.js; !(forever list | grep -q build.server.js) && forever start build/server.js;"
