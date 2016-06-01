53deeb09bfeeadfb615a8b36c694cab0b239974a
maximHome <maxstbn@gmail.com>
2016-06-01 15:08:59 +0300

    поменял порт для боя

--- bin/config.js
+++ bin/config.js
@@ -160,7 +160,7 @@ var config = {
             "beta": "http://api.beta.inna.ru"
         },
         "port": {
-            "prod": 8669,
+            "prod": 8666,
             "test": 8667,
             "dev": 8667,
             "beta": 8668
