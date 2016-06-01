1982304826aae7bb8cca0f23e449bce4439a7147
maximHome <maxstbn@gmail.com>
2016-06-01 16:39:27 +0300

    поправил видеоменеджер

--- spa/js/components/manager/manager.js
+++ spa/js/components/manager/manager.js
@@ -1,58 +1,22 @@
-innaAppDirectives.directive('manager', function ($templateCache, $interval, $timeout, ManagerService) {
+innaAppDirectives.directive('manager', function ($rootScope, $templateCache, $interval, $timeout) {
     return {
         replace: true,
         template: $templateCache.get("components/manager/templ/index.html"),
         link: function ($scope, element, attrs) {
-            var url = "//manager.inna.ru/";
 
-            $scope.url = url;
             $scope.showChat = false;
             $scope.showChatManager = false;
 
-            function setManager () {
-                console.log('getManagerStatus', $scope.showChat)
-                ManagerService.getManagerStatus()
-                    .then(function (res) {
-                        if (res) {
-                            $scope.showChat = true;
-                            $timeout(function () {
-                                $scope.showChatManager = true;
-                            }, 10000)
-                        }else{
-                            $scope.showChat = false;
-                            $scope.showChatManager = false;
-                        }
-                    }, function (res) {
-                        $scope.showChat = false;
-                        $scope.showChatManager = false;
-                    });
-            }
-
-            setManager();
-
-
-            var stop;
-            $scope.fight = function () {
-                // Don't start a new fight if we are already fighting
-                if (angular.isDefined(stop)) return;
-
-                stop = $interval(function () {
-                    setManager();
-                }, 4000);
-            };
-
-            $scope.stopFight = function () {
-                if (angular.isDefined(stop)) {
-                    $interval.cancel(stop);
-                    stop = undefined;
+            $scope.$on('showManager', function (event, data) {
+                if (data) {
+                    $scope.showChat = true;
+                    $timeout(function () {
+                        $scope.showChatManager = true;
+                    }, 10000)
+                } else {
+                    $scope.showChat = false;
+                    $scope.showChatManager = false;
                 }
-            };
-
-            $scope.fight();
-
-            $scope.$on('$destroy', function () {
-                // Make sure that the interval is destroyed too
-                $scope.stopFight();
             });
 
             //fullWidth
@@ -84,8 +48,52 @@ innaAppDirectives.directive('managerWidget', function ($templateCache, $interval
             $scope.url = url;
             $scope.openWidget = false;
             $scope.openFullWidth = false;
-
             $scope.showChat = false;
+
+
+            function setManager() {
+                console.log('getManagerStatus', $scope.showChat)
+                ManagerService.getManagerStatus()
+                    .then(function (res) {
+                        if (res) {
+                            $rootScope.$broadcast('showManager', true);
+                            $scope.showChat = true;
+                        } else {
+                            $rootScope.$broadcast('showManager', false);
+                            $scope.showChat = false;
+                        }
+                    }, function (res) {
+                        $rootScope.$broadcast('showManager', false);
+                        $scope.showChat = false;
+                    });
+            }
+
+            setManager();
+
+
+            var stop;
+            $scope.fight = function () {
+                // Don't start a new fight if we are already fighting
+                if (angular.isDefined(stop)) return;
+
+                stop = $interval(function () {
+                    setManager();
+                }, 30000);
+            };
+
+            $scope.stopFight = function () {
+                if (angular.isDefined(stop)) {
+                    $interval.cancel(stop);
+                    stop = undefined;
+                }
+            };
+
+            $scope.fight();
+
+            $scope.$on('$destroy', function () {
+                // Make sure that the interval is destroyed too
+                $scope.stopFight();
+            });
 
 
             $scope.toggleOpenWidget = function () {
@@ -101,47 +109,6 @@ innaAppDirectives.directive('managerWidget', function ($templateCache, $interval
                 $scope.openFullWidth = !$scope.openFullWidth;
             };
 
-
-            function setManager () {
-                console.log('getManagerStatus widget', $scope.showChat)
-                ManagerService.getManagerStatus()
-                    .then(function (res) {
-                        if (res) {
-                            $scope.showChat = true;
-                        }else{
-                            $scope.showChat = false;
-                        }
-                    }, function (res) {
-                        $scope.showChat = false;
-                    });
-            }
-
-            setManager();
-
-            var stopWidget;
-            $scope.fightWidget = function () {
-                // Don't start a new fightWidget if we are already fighting
-                if (angular.isDefined(stopWidget)) return;
-
-                stopWidget = $interval(function () {
-                    setManager();
-                }, 6000);
-            };
-
-            $scope.stopFightWidget = function () {
-                if (angular.isDefined(stopWidget)) {
-                    $interval.cancel(stopWidget);
-                    stopWidget = undefined;
-                }
-            };
-
-            $scope.fightWidget();
-
-
-            $scope.$on('$destroy', function () {
-                // Make sure that the interval is destroyed too
-                $scope.stopFightWidget();
-            });
         }
     }
 });
