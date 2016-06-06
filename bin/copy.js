af62b2f49d69e73e1b9f9de6c3689f0ec6506e14
maximHome <maxstbn@gmail.com>
2016-06-06 10:51:58 +0300

    Добавил в сборку копирование шаблонов для оплаты

--- bin/copy.js
+++ bin/copy.js
@@ -9,9 +9,9 @@ gulp.task('copy-js', function () {
     gulp.src('./spa/lib/if-browser.js').pipe(gulp.dest(config.js.distSrc));
     gulp.src('./spa/js/partners/partners.js').pipe(gulp.dest(config.js.distSrc));
     gulp.src('./spa/partners/**').pipe(gulp.dest('./dist/spa/partners'));
-    gulp.src('./spa/*.html').pipe(gulp.dest('./dist/spa'));
     gulp.src('./spa/*.css').pipe(gulp.dest('./dist/spa'));
     gulp.src('./spa/*.docx').pipe(gulp.dest('./dist/spa'));
+    gulp.src('./spa/templates/pages/**/*.html').pipe(gulp.dest('./dist/spa/templates/pages'));
 });
 
 gulp.task('copy-img', function () {
