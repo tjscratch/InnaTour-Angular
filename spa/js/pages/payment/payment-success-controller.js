045745c2b68facc9014777270719deb942352577
maximHome <maxstbn@gmail.com>
2016-08-05 16:47:10 +0300

    страница при успешной оплате

--- spa/js/pages/payment/payment-success-controller.js
+++ spa/js/pages/payment/payment-success-controller.js
@@ -2,7 +2,7 @@
  * во вьюхе используем как
  * ReservationsController as reservation
  */
-innaAppControllers.controller('PaymentSuccessController', function ($routeParams, Payment, EventManager, innaAppApiEvents) {
+innaAppControllers.controller('PaymentSuccessController', function (EventManager, innaAppApiEvents) {
     
     var self = this;
     
@@ -10,51 +10,6 @@ innaAppControllers.controller('PaymentSuccessController', function ($routeParams
     $(".zopim").hide();
     $("body").removeClass("light-theme");
     EventManager.fire(innaAppApiEvents.FOOTER_HIDDEN);
-    
-    self.OrderNum = $routeParams.OrderNum;
-    
-    /**
-     * получение от сервера данных для оплаты
-     */
-    Payment.getPaymentData({orderNum: $routeParams.OrderNum})
-        .then(
-            getPaymentDataSuccess,
-            getPaymentDataError
-        );
-    
-    
-    /**
-     * успешная загрузка данных для оплаты
-     * установка типа продукта
-     * dp, avia, hotel
-     * по умолчанию dp
-     */
-    function getPaymentDataSuccess(response) {
-        var data = response.data;
-        self.data = data;
-        self.data.passengerCount = self.data.Passengers.length;
-        
-        // хрень для определения типа продукта
-        // надо бы выпилить и заставить бэкенд присылать это в апи
-        if (data.Hotel != null && data.AviaInfo != null) {
-            self.productType = 'dp';
-        } else if (data.Hotel == null && data.AviaInfo != null) {
-            self.productType = 'avia';
-        } else if (data.Hotel != null && data.AviaInfo == null) {
-            self.productType = 'hotel';
-        }
-    }
-    
-    
-    /**
-     * при загрузке данных произошла ошибка
-     * коллбек для обработки этой ошибки
-     */
-    function getPaymentDataError(response) {
-        console.error('Произошла ошибка', response);
-    }
-    
-    
     
     
 });
