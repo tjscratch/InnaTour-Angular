438d4e176ac335fddcc5ef9e582dc9b80816c30f
maximHome <maxstbn@gmail.com>
2016-09-19 11:13:09 +0300

    переключалка на мобильную версию

--- bin/config.js
+++ bin/config.js
@@ -158,7 +158,7 @@ var config = {
         },
         "api": {
             "test": "http://api.test.inna.ru",
-            "dev": "http://lh.inna.ru",
+            "dev": "http://test.inna.ru",
             "prod": "https://api.inna.ru",
             "beta": "http://api.beta.inna.ru"
         },
--- spa/js/regions/header/header.js
+++ spa/js/regions/header/header.js
@@ -12,9 +12,10 @@ innaAppControllers
         'innaApp.Urls',
         'AppRouteUrls',
         'innaAppApiEvents',
+        'serviceCache',
         'aviaHelper',
         '$timeout',
-        function (EventManager, $rootScope, $scope, $element, $location, eventsHelper, urlHelper, appUrls, AppRouteUrls, Events, aviaHelper, $timeout) {
+        function (EventManager, $rootScope, $scope, $element, $location, eventsHelper, urlHelper, appUrls, AppRouteUrls, Events, serviceCache, aviaHelper, $timeout) {
             
             var partner = window.partners ? window.partners.getPartner() : null;
             if (partner != null && partner.name == 'sputnik') {
@@ -23,8 +24,27 @@ innaAppControllers
             else {
                 $scope.headerTemplateSrc = 'regions/header/templ/header.html';
             }
-            
-            
+    
+            $(window).on('unload beforeunload', function () {
+                serviceCache.drop('isMobile');
+            });
+            var md = new MobileDetect(window.navigator.userAgent);
+            if (serviceCache.getObject('isMobile') != 'hide') {
+                serviceCache.createObj('isMobile', 'show');
+                $scope.isMobile = md.mobile();
+                document.body.classList.add('inject-toggle-mobile');
+            }
+            // $scope.isMobile = md.mobile();
+            $scope.isMobileClose = function () {
+                $scope.isMobile = false;
+                serviceCache.createObj('isMobile', 'hide');
+                document.body.classList.remove('inject-toggle-mobile');
+            };
+    
+            $scope.isMobile = true;
+            document.body.classList.add('inject-toggle-mobile');
+    
+    
             /**
              * Отели у нас работают только для b2b клиентов
              * поэтому если не b2b пользователь попал на страницу отелей
@@ -82,15 +102,15 @@ innaAppControllers
             });
             
             $scope.urls = appUrls;
-
+            
             $scope.gtmMainTab = function ($event) {
                 // console.log('$location.absUrl()', $location.absUrl());
                 // console.log('$location.path()', $location.path());
                 var loc = $location.path();
                 var abs = $location.absUrl();
-
+                
                 var category = '';
-
+                
                 var isDynamic = (
                         loc.startsWith(appUrls.URL_DYNAMIC_PACKAGES) && !loc.startsWith(appUrls.URL_DYNAMIC_PACKAGES_RESERVATION) && !loc.startsWith(appUrls.URL_DYNAMIC_PACKAGES_BUY)
                     ) || loc == appUrls.URL_ROOT;
@@ -109,16 +129,16 @@ innaAppControllers
                 else if (loc.startsWith(AppRouteUrls.URL_BUS)) {
                     category = 'Bus';
                 }
-
+                
                 var dataLayerObj = {
                     'event': 'UM.Event',
-                    'Data': {
+                    'Data' : {
                         'Category': category,
-                        'Action': 'MainTab',
-                        'Label': $event.target.textContent,
-                        'Content': '[no data]',
-                        'Context': '[no data]',
-                        'Text': '[no data]'
+                        'Action'  : 'MainTab',
+                        'Label'   : $event.target.textContent,
+                        'Content' : '[no data]',
+                        'Context' : '[no data]',
+                        'Text'    : '[no data]'
                     }
                 };
                 console.table(dataLayerObj);
