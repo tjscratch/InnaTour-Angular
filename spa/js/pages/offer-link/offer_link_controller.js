d507f1538ae715946ec71a808c2d958e1b21e2da
maximHome <maxstbn@gmail.com>
2016-09-28 01:19:13 +0300

    разработка редиректа по ссылке с предложением

--- spa/js/pages/offer-link/offer_link_controller.js
+++ spa/js/pages/offer-link/offer_link_controller.js
@@ -3,7 +3,7 @@ innaAppControllers.controller('OfferLinkController', function ($scope, $routePar
     // localhost:3000/#/select?DepartureId=6733&ArrivalId=3005&Adult=2&StartDate=2016-11-13&Nights=8&Host=https://inna.ru&TicketClass=0&HotelName=&Price=64401&Stars=5
     
     var baloon = aviaHelper.baloon;
-    baloon.show('Собираем данные', 'Это может занять несколько секунд');
+    baloon.show('Собираем данные', 'Это может занять какое-то время');
     
     dataService.getOfferLink($routeParams)
         .then(
