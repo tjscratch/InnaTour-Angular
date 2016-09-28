55849a24c5696cde0da587d6db1b4167144e5174
maximHome <maxstbn@gmail.com>
2016-09-28 18:05:30 +0300

    разработка редиректа по ссылке с предложением

--- spa/js/pages/offer-link/offer_link_controller.js
+++ spa/js/pages/offer-link/offer_link_controller.js
@@ -1,6 +1,7 @@
 innaAppControllers.controller('OfferLinkController', function ($scope, $routeParams, $location, AppRouteUrls, aviaHelper, dataService) {
     
     // localhost:3000/#/select?DepartureId=6733&ArrivalId=3005&Adult=2&StartDate=2016-11-13&Nights=8&Host=https://inna.ru&TicketClass=0&HotelName=&Price=64401&Stars=5
+    // https://inna.ru/#/select?DepartureId=6733&ArrivalId=3005&Adult=2&StartDate=2016-11-13&Nights=8&Host=https://inna.ru&TicketClass=0&HotelName=&Price=64401&Stars=5
     // http://lh.inna.ru/wl-test/1001tur/#/select?DepartureId=6733&ArrivalId=3005&Adult=2&StartDate=2016-11-13&Nights=8&Host=https://inna.ru&TicketClass=0&HotelName=&Price=64401&Stars=5
     
     
@@ -16,9 +17,9 @@ innaAppControllers.controller('OfferLinkController', function ($scope, $routePar
                     // $location.url(res.data);
                     
                     var urlDetails = res.data;
-                    if (window.partners && window.partners.isFullWL()) {
-                        urlDetails = window.partners.getParentLocationWithUrl(urlDetails);
-                    }
+                    // if (window.partners && window.partners.isFullWL()) {
+                    //     urlDetails = window.partners.getParentLocationWithUrl(urlDetails);
+                    // }
                     $location.url(urlDetails);
                     baloon.hide();
                 }
