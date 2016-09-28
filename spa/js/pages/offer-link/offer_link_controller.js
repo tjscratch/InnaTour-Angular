8ba61561404e739e6e7c7c5fed8a8f0962835138
maximHome <maxstbn@gmail.com>
2016-09-28 16:23:03 +0300

    разработка редиректа по ссылке с предложением

--- spa/js/pages/offer-link/offer_link_controller.js
+++ spa/js/pages/offer-link/offer_link_controller.js
@@ -1,15 +1,25 @@
 innaAppControllers.controller('OfferLinkController', function ($scope, $routeParams, $location, AppRouteUrls, aviaHelper, dataService) {
     
     // localhost:3000/#/select?DepartureId=6733&ArrivalId=3005&Adult=2&StartDate=2016-11-13&Nights=8&Host=https://inna.ru&TicketClass=0&HotelName=&Price=64401&Stars=5
+    // http://lh.inna.ru/wl-test/1001tur/#/select?DepartureId=6733&ArrivalId=3005&Adult=2&StartDate=2016-11-13&Nights=8&Host=https://inna.ru&TicketClass=0&HotelName=&Price=64401&Stars=5
+    
     
     var baloon = aviaHelper.baloon;
     baloon.show('Собираем данные', 'Это может занять какое-то время');
     
+    
     dataService.getOfferLink($routeParams)
         .then(
             function (res) {
-                if (res.data){
-                    window.location = res.data;
+                if (res.data) {
+                    // window.location.href = res.data;
+                    // $location.url(res.data);
+                    
+                    var urlDetails = res.data;
+                    if (window.partners && window.partners.isFullWL()) {
+                        urlDetails = window.partners.getParentLocationWithUrl(urlDetails);
+                    }
+                    $location.url(urlDetails);
                     baloon.hide();
                 }
             },
