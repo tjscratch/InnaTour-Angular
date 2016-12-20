c85c8eae563e7b3825ca9958a525ed050a1f1df8
Pavel Malyshev <p.malyshev@inna.ru>
2016-12-20 14:56:13 +0300

    Revert "Вход с формы"
    This reverts commit b667788451c31da3da4c9ff6cb4381cf9091d0ef

--- spa/js/components/filter-panel/filter-settings.js
+++ spa/js/components/filter-panel/filter-settings.js
@@ -128,7 +128,7 @@ angular.module('innaApp.services')
                                     value: 'byRecommend'
                                 },
                                 {
-                                    name: 'Время в  пути',
+                                    name: 'Врумя в  пути',
                                     value: 'byTripTime'
                                 },
                                 // {
